b2524f6a720b26c75064d1b02c2f906ad0af3053
Julien Blanchet <julien@rezo-zero.com>
2014-10-24 18:26:11 +0200

    Tree panel style

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-                '/style.7e449a55.css',
+                '/style.8f324e52.css',
             ),
             'js' => array(
                 '/rozier-backend-theme.a5c8a25a.min.js',
