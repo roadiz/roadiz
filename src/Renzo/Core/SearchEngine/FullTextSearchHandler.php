ba7435cafb8e0a1d1da876a1f415b62bc88cb575
maxime constantinian <maxime.constantinian@gmail.com>
2014-10-28 18:55:29 +0100

    add node argument for searching

--- src/Renzo/Core/SearchEngine/FullTextSearchHandler.php
+++ src/Renzo/Core/SearchEngine/FullTextSearchHandler.php
@@ -24,9 +24,10 @@ class FullTextSearchHandler
 {
     protected $client = null;
 
-    public function __construct($client)
+    public function __construct($client = null)
     {
         $this->client = $client;
+        $this->client = Kernel::getService("solr");
     }
 
     private function solrSearch($q, $args = [])
@@ -34,7 +35,13 @@ class FullTextSearchHandler
         $query = $this->client->createSelect();
         $query->setQuery($q);
         foreach ($args as $key => $value) {
-            $query->addParam($key, $value);
+            if (is_array($value)){
+                foreach ($value as $k => $v) {
+                    $query->addFilterQuery(array("key" => "fq".$k, "query"=>$v));
+                }
+            } else {
+                $query->addParam($key, $value);
+            }
         }
 
         $resultset = $this->client->select($query);
@@ -52,10 +59,40 @@ class FullTextSearchHandler
                         return Kernel::getInstance()->getService('em')->find('RZ\Renzo\Core\Entities\NodesSources', $n["node_source_id_i"]);
                     },
                     $reponse['response']['docs']);
+        var_dump($this->client->createRequest($query)->getUri());
         echo '<pre>';
         \Doctrine\Common\Util\Debug::dump($doc, 10, false);
         echo '</pre>';
         return $doc;
+    }
+
+    private function argFqProcess(&$args)
+    {
+        if (!isset($args["fq"])) {
+            $args["fq"] = array();
+        }
+        if (isset($args['visible'])) {
+            $tmp = "node_visible_b:".(($args['visible']) ? 'true' : 'false');
+            unset($args['visible']);
+            $args["fq"][] = $tmp;
+        } else {
+            $args["fq"][] = "node_visible_b:true";
+        }
+        if (isset($args['status'])) {
+            $tmp = "node_status_i:";
+            if (!is_array($args['status'])) {
+                $tmp .= (string)$args['status'];
+            } elseif ($args['status'][0] == "<=") {
+                $tmp .= "[* TO " . (string)$args['status'][1] . "]";
+            } elseif ($args['status'][0] == ">=") {
+                $tmp .= "[" . (string)$args['status'][1] . " TO *]";
+            }
+            unset($args['status']);
+            $args["fq"][] = $tmp;
+        } else {
+            $args["fq"][] = "node_status_i:".(string)(Node::PUBLISHED);
+        }
+        return $args;
     }
 
     /**
@@ -73,8 +110,9 @@ class FullTextSearchHandler
      */
     public function searchWithHighlight($q, $args = array())
     {
+        $args = $this->argFqProcess($args);
+        $args["fq"][] = "document_type_s:NodesSources";
         $tmp = array();
-        $tmp["fq"] = "document_type_s:NodesSources";
         $tmp["hl"] = true;
         $tmp["hl.fl"] = "*";
         $tmp["hl.simple.pre"] = '<span class="solr-highlight">';
@@ -99,9 +137,17 @@ class FullTextSearchHandler
      */
     public function search($q, $args = array())
     {
+        $args = $this->argFqProcess($args);
+        $args["fq"][] = "document_type_s:NodesSources";
         $tmp = array();
-        $tmp["fq"] = "document_type_s:NodesSources";
         $args = array_merge($tmp, $args);
         return $this->solrSearch($q, $args);
+    }
+
+    public function searchAction(Request $request, $q)
+    {
+        $args["visible"] = true;
+        $args["status"] = array(">=", 30);
+        $this->search($q, $args);
     }
 }
