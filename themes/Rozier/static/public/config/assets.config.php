8fe6ca3195703d7a97ed0a7a73320fa5f6a4fdcc
Ambroise Maupate <ambroise@rezo-zero.com>
2014-10-29 14:00:09 +0100

    Css for notify and simple titles on node tree

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-                '/style.768f6e82.css',
+                '/style.8c00d2f4.css',
             ),
             'js' => array(
                 '/rozier-backend-theme.7194c355.min.js',
