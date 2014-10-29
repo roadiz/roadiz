b1bfe73fb5d9600828fc507a8267788f14e70f3a
Julien Blanchet <julien@rezo-zero.com>
2014-10-29 16:17:08 +0100

    grunt

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -3,17 +3,10 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-<<<<<<< HEAD
-                '/style.9853eb23.css',
+                '/style.77aa3bb4.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.d2d65132.min.js',
-=======
-                '/style.cd8f15e6.css',
-            ),
-            'js' => array(
-                '/rozier-backend-theme.59de517a.min.js',
->>>>>>> feature/search
+                '/rozier-backend-theme.2df43786.min.js',
             ),
         ),
     )
