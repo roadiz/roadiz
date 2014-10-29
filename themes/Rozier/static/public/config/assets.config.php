864bf409627cf3db10921e6a8feb33b1e28a4bb0
Maxime Bérard <maxime@rezo-zero.com>
2014-10-29 14:38:47 +0100

    Stylized children node widget & documents widget

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-                '/style.b9ecceb9.css',
+                '/style.41684f83.css',
             ),
             'js' => array(
                 '/rozier-backend-theme.d8d6b1d6.min.js',
