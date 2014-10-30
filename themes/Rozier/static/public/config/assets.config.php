144b47e067423c425129d5aa8a766e16bc1f1549
Julien Blanchet <julien@rezo-zero.com>
2014-10-30 16:11:10 +0100

    Change documents thumbnail

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-                '/style.3376dbb1.css',
+                '/style.31582639.css',
             ),
             'js' => array(
                 '/rozier-backend-theme.8b361958.min.js',
