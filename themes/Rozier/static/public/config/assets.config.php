fd72b8a7942d6d79b615740b4c8bf9e43cbaee79
Julien Blanchet <julien@rezo-zero.com>
2014-11-05 16:50:26 +0100

    Change tree style

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-                '/style.3731cf39.css',
+                '/style.283a474e.css',
             ),
             'js' => array(
                 '/rozier-backend-theme.3f97c5c3.min.js',
