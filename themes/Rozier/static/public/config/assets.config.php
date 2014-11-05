3367aa9b0a8189eea488e1f1a5bb2ef60522645f
Maxime Bérard <maxime@rezo-zero.com>
2014-11-05 17:57:28 +0100

    Harmonized list pages (edit, save buttons, icons ,etc.)

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-                '/style.4d6167f6.css',
+                '/style.226848e3.css',
             ),
             'js' => array(
                 '/rozier-backend-theme.8583420a.min.js',
