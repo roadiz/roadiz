6c7ca07133370f03b3145c612ab58e2bbaef5cde
Maxime Bérard <maxime@rezo-zero.com>
2014-11-06 17:17:17 +0100

    Diverse styles, responsive, table list pages

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-                '/style.f419c756.css',
+                '/style.b8c5dcd3.css',
             ),
             'js' => array(
                 '/rozier-backend-theme.c0da4757.min.js',
