2a9302472813a7b3ebac89666782fb228be0e060
Julien Blanchet <julien@rezo-zero.com>
2014-11-11 13:20:58 +0100

    Change Style node tree widget

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-                '/style.05f14114.css',
+                '/style.3b35fdd1.css',
             ),
             'js' => array(
                 '/rozier-backend-theme.f3beec45.min.js',
