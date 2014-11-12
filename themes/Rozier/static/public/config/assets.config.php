eee6923f75dabbfc978769dabc489753a00378f4
Maxime Bérard <maxime@rezo-zero.com>
2014-11-12 14:00:09 +0100

    Changed styles for table buttons (save, delete)

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-                '/style.deed1558.css',
+                '/style.86c2e496.css',
             ),
             'js' => array(
                 '/rozier-backend-theme.63d2dfb1.min.js',
