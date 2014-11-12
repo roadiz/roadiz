c70dfb603cc6b38f3e53835093550fc4189c0f38
Maxime Bérard <maxime@rezo-zero.com>
2014-11-12 11:44:28 +0100

    Fixes #166 : rename buttons labels ( ex : add a node, add node, etc.), styles on custom forms pages

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-                '/style.8f305c1d.css',
+                '/style.deed1558.css',
             ),
             'js' => array(
                 '/rozier-backend-theme.63d2dfb1.min.js',
