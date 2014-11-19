33c8cb4bc91d3ec67718ab77d328134710ed4201
Ambroise Maupate <ambroise@rezo-zero.com>
2014-11-19 17:40:30 +0100

    Some primary style for bulk actions

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-                '/style.780b1f7d.css',
+                '/style.8dc93295.css',
             ),
             'js' => array(
                 '/rozier-backend-theme.1dd5de07.min.js',
