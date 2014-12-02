c5db59b676e91890285939252e4f5744d9c8912d
Ambroise Maupate <ambroise@rezo-zero.com>
2014-12-02 11:16:48 +0100

    Factorized building query comparison

--- src/Roadiz/Core/Repositories/DocumentRepository.php
+++ src/Roadiz/Core/Repositories/DocumentRepository.php
@@ -168,70 +168,7 @@ class DocumentRepository extends EntityRepository
                 $prefix = 'dt.';
             }
 
-            if (is_object($value) && $value instanceof PersistableInterface) {
-                $res = $qb->expr()->eq($prefix.$key, ':'.$baseKey);
-            } elseif (is_array($value)) {
-                /*
-                 * array
-                 *
-                 * ['<=', $value]
-                 * ['<', $value]
-                 * ['>=', $value]
-                 * ['>', $value]
-                 * ['BETWEEN', $value, $value]
-                 * ['LIKE', $value]
-                 * in [$value, $value]
-                 */
-                if (count($value) > 1) {
-                    switch ($value[0]) {
-                        case '<=':
-                            # lte
-                            $res = $qb->expr()->lte($prefix.$key, ':'.$baseKey);
-                            break;
-                        case '<':
-                            # lt
-                            $res = $qb->expr()->lt($prefix.$key, ':'.$baseKey);
-                            break;
-                        case '>=':
-                            # gte
-                            $res = $qb->expr()->gte($prefix.$key, ':'.$baseKey);
-                            break;
-                        case '>':
-                            # gt
-                            $res = $qb->expr()->gt($prefix.$key, ':'.$baseKey);
-                            break;
-                        case 'BETWEEN':
-                            $res = $qb->expr()->between(
-                                $prefix.$key,
-                                ':'.$baseKey.'_1',
-                                ':'.$baseKey.'_2'
-                            );
-                            break;
-                        case 'LIKE':
-                            $res = $qb->expr()->like($prefix.$key, $qb->expr()->literal($value[1]));
-                            break;
-                        case 'NOT IN':
-                            $res = $qb->expr()->notIn($prefix.$key, ':'.$baseKey);
-                            break;
-                        default:
-                            $res = $qb->expr()->in($prefix.$key, ':'.$baseKey);
-                            break;
-                    }
-                } else {
-                    $res = $qb->expr()->in($prefix.$key, ':'.$baseKey);
-                }
-
-            } elseif (is_bool($value)) {
-                $res = $qb->expr()->eq($prefix.$key, ':'.$baseKey);
-            } elseif ('NOT NULL' == $value) {
-                $res = $qb->expr()->isNotNull($prefix.$key);
-            } elseif (isset($value)) {
-                $res = $qb->expr()->eq($prefix.$key, ':'.$baseKey);
-            } elseif (null === $value) {
-                $res = $qb->expr()->isNull($prefix.$key);
-            }
-
-            $qb->andWhere($res);
+            $qb->andWhere($this->buildComparison($value, $prefix, $key, $baseKey, $qb));
         }
     }
 
