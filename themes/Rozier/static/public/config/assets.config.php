5783259ea2542f817b2e67b6b66b6e2713c1b12f
Maxime Bérard <maxime@rezo-zero.com>
2014-12-04 18:31:19 +0100

    Fix nodetree home icon position if is root or children

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -3,11 +3,11 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-                '/style.297b3cd4.css',
+                '/style.6364b323.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.2d0d07a4.min.js',
-                '/rozier-backend-theme-vendor.f9cfdd85.min.js',
+                '/rozier-backend-theme.493c7be4.min.js',
+                '/rozier-backend-theme-vendor.958b08cc.min.js',
             ),
         ),
     )
