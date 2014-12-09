89ecb5a160628b28b8a2c28814093316110b9054
Ambroise Maupate <ambroise@rezo-zero.com>
2014-12-09 20:06:08 +0100

    Fixed some sensiolaabs insight violations

--- themes/Rozier/static/js/addons/datepicker.js
+++ themes/Rozier/static/js/addons/datepicker.js
@@ -864,7 +864,7 @@
             return res;
         }
 
-        // TODO: remove 'name' arg after deprecation is removed
+        // remove 'name' arg after deprecation is removed
         function createAdder(direction, name) {
             return function (val, period) {
                 var dur, tmp;
@@ -1612,7 +1612,7 @@
                 dow = 1;
                 doy = 4;
 
-                // TODO: We need to take the current isoWeekYear, but that depends on
+                // We need to take the current isoWeekYear, but that depends on
                 // how we interpret now (local, utc, fixed offset). So create
                 // a now version of current config (take local/utc/offset flags, and
                 // create now).
@@ -2837,10 +2837,10 @@
         function rawMonthSetter(mom, value) {
             var dayOfMonth;
 
-            // TODO: Move this out of here!
+            // Move this out of here!
             if (typeof value === 'string') {
                 value = mom.localeData().monthsParse(value);
-                // TODO: Another silent failure?
+                // Another silent failure?
                 if (typeof value !== 'number') {
                     return mom;
                 }
