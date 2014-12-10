dbf858e5fe01e4705d23a043b9fa6390a7847fb5
Maxime Bérard <maxime@rezo-zero.com>
2014-12-10 15:30:34 +0100

    Forgot to stop grunt watch

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -6,7 +6,7 @@ return array(
                 '/style.8b379f95.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.7d226afc.min.js',
+                '/rozier-backend-theme.d01a9e10.min.js',
                 '/rozier-backend-theme-vendor.f016e1be.min.js',
             ),
         ),
