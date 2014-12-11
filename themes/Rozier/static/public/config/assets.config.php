3f0f5508d314392fd655e40dc8d55b9ddd1c49e8
Maxime Bérard <maxime@rezo-zero.com>
2014-12-11 15:25:55 +0100

    Fixed node type button alignment in node search page

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -3,11 +3,11 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-                '/style.5a351038.css',
+                '/style.bb361da3.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.8fc2a00b.min.js',
-                '/rozier-backend-theme-vendor.c7a226b6.min.js',
+                '/rozier-backend-theme.20dcc052.min.js',
+                '/rozier-backend-theme-vendor.76ecb9d0.min.js',
             ),
         ),
     )
