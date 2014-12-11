d30b2f2bcf8de66c129e9de262b6ab08787613e5
Julien Blanchet <julien@rezo-zero.com>
2014-12-11 17:14:10 +0100

    Style Install Step 1

--- themes/Install/static/public/config/assets.config.php
+++ themes/Install/static/public/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-                '/style.d6fafcde.css',
+                '/style.22a2d679.css',
             ),
             'js' => array(
                 '/install-theme.85a7d6c0.min.js',
