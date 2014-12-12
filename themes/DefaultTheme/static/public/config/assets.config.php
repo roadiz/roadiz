72fb69c800bbf819396bfeeb1e59cbf399b4f495
Julien Blanchet <julien@rezo-zero.com>
2014-12-12 18:44:13 +0100

    Change default theme in progress

--- themes/DefaultTheme/static/public/config/assets.config.php
+++ themes/DefaultTheme/static/public/config/assets.config.php
@@ -3,7 +3,7 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-                '/style.2dce1245.css',
+                '/style.138b869c.css',
             ),
             'js' => array(
                 '/default-theme.3e34d379.min.js',
