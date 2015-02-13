9b129b10fe70d67c59d4781543f8a24e0c01f234
Ambroise Maupate <ambroise@rezo-zero.com>
2015-02-13 16:26:37 +0100

    Improved admin logo rendering.

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -3,11 +3,11 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-                '/style.5901c392.css',
+                '/style.177a334e.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.57a01198.min.js',
-                '/rozier-backend-theme-vendor.c3fc392c.min.js',
+                '/rozier-backend-theme.8af36066.min.js',
+                '/rozier-backend-theme-vendor.76d83a1e.min.js',
             ),
         ),
     )
