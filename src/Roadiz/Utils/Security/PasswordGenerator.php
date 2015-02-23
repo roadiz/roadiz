da48abfcd56042c38ec8b3d2870a1bfc4cd2a212
Ambroise Maupate <ambroise@rezo-zero.com>
2015-02-23 15:21:02 +0100

    Simplified passwordGenerator.

--- src/Roadiz/Utils/Security/PasswordGenerator.php
+++ src/Roadiz/Utils/Security/PasswordGenerator.php
@@ -37,15 +37,9 @@ class PasswordGenerator extends RandomGenerator implements PasswordGeneratorInte
      * in the password are chosen at random from those four sets.
      *
      * The available characters in each set are user friendly - there are no ambiguous
-     * characters such as i, l, 1, o, 0, etc. This, coupled with the $add_dashes option,
-     * makes it much easier for users to manually type or speak their passwords.
-     *
-     * Note: the $add_dashes option will increase the length of the password by
-     * floor(sqrt(N)) characters.
+     * characters such as i, l, 1, o, 0, etc.
      *
      * @param  integer $length
-     * @param  boolean $add_dashes
-     * @param  string  $available_sets
      *
      * @return string
      *
@@ -53,22 +47,11 @@ class PasswordGenerator extends RandomGenerator implements PasswordGeneratorInte
      */
     public function generatePassword($length = 9)
     {
-        $add_dashes = false;
-        $available_sets = 'luds';
-
-        $sets = array();
-        if (strpos($available_sets, 'l') !== false) {
-            $sets[] = 'abcdefghjkmnpqrstuvwxyz';
-        }
-        if (strpos($available_sets, 'u') !== false) {
-            $sets[] = 'ABCDEFGHJKMNPQRSTUVWXYZ';
-        }
-        if (strpos($available_sets, 'd') !== false) {
-            $sets[] = '23456789';
-        }
-        if (strpos($available_sets, 's') !== false) {
-            $sets[] = '!@#$%&*?';
-        }
+        $sets = [];
+        $sets[] = 'abcdefghjkmnpqrstuvwxyz';
+        $sets[] = 'ABCDEFGHJKMNPQRSTUVWXYZ';
+        $sets[] = '23456789';
+        $sets[] = '!@#$%&*?';
 
         $all = '';
         $password = '';
@@ -84,17 +67,6 @@ class PasswordGenerator extends RandomGenerator implements PasswordGeneratorInte
 
         $password = str_shuffle($password);
 
-        if (!$add_dashes) {
-            return $password;
-        }
-
-        $dash_len = floor(sqrt($length));
-        $dash_str = '';
-        while (strlen($password) > $dash_len) {
-            $dash_str .= substr($password, 0, $dash_len) . '-';
-            $password = substr($password, $dash_len);
-        }
-        $dash_str .= $password;
-        return $dash_str;
+        return $password;
     }
 }
