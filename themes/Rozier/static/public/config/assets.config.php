da0463cca92bfc1b1941d7566b9520de113198dd
Ambroise Maupate <ambroise@rezo-zero.com>
2015-02-24 17:56:03 +0100

    Added special icon for a Stack-home and non-published stack.

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -3,11 +3,11 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-                '/style.231c2589.css',
+                '/style.4607aba3.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.f7ed4c80.min.js',
-                '/rozier-backend-theme-vendor.5ff9b612.min.js',
+                '/rozier-backend-theme.929be4e3.min.js',
+                '/rozier-backend-theme-vendor.3a7f9ab5.min.js',
             ),
         ),
     )
