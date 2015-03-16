059cb35ef43e5a2d25dd4ff5d556c431d0f3d662
Ambroise Maupate <ambroise@rezo-zero.com>
2015-03-16 10:52:26 +0100

    Fixed switches and radio buttons. Delete temp js files

--- themes/Rozier/static/js/widgets/nodeStatuses.js
+++ themes/Rozier/static/js/widgets/nodeStatuses.js
@@ -25,6 +25,7 @@ NodeStatuses.prototype.init = function() {
     _this.$inputs.on('change', $.proxy(_this.onChange, _this));
 
     _this.$containers.find(".rz-boolean-checkbox").bootstrapSwitch({
+        size: 'small',
         "onSwitchChange": $.proxy(_this.onChange, _this)
     });
 };
--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -3,28 +3,28 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-                '/vendor.50ccbfeb.css',
+                '/vendor.285e0042.css',
                 '/style.3a0bec42.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.4c94f004.min.js',
-                '/rozier-backend-theme-vendor.e51e544b.min.js',
+                '/rozier-backend-theme.37ffb2de.min.js',
+                '/rozier-backend-theme-vendor.942d5de1.min.js',
             ),
         ),
         'simple' => array(
             'css' => array(
             ),
             'js' => array(
-                '/rozier-backend-theme-simple.c43b9b85.min.js',
+                '/rozier-backend-theme-simple.da4dc2e7.min.js',
             ),
         ),
         'custom-forms' => array(
             'css' => array(
-                '/vendor.50ccbfeb.css',
+                '/vendor.285e0042.css',
                 '/custom-forms-front.205b8654.css',
             ),
             'js' => array(
-                '/rozier-backend-theme-cforms.584ca701.min.js',
+                '/rozier-backend-theme-cforms.477e9dee.min.js',
             ),
         ),
     )
