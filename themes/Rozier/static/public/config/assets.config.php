344110313bd855abddf556f54db4106c487d3898
Ambroise Maupate <ambroise.maupate@gmail.com>
2015-03-18 03:16:36 +0100

    Used minified CSS vendor for Rozier theme

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -3,28 +3,28 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-                '/vendor.8975c8e4.css',
+                '/vendor.6a0825ea.css',
                 '/style.81eddce1.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.3515aa33.min.js',
-                '/rozier-backend-theme-vendor.35c4a904.min.js',
+                '/rozier-backend-theme.2a72ed00.min.js',
+                '/rozier-backend-theme-vendor.4c357fc4.min.js',
             ),
         ),
         'simple' => array(
             'css' => array(
             ),
             'js' => array(
-                '/rozier-backend-theme-simple.b8908085.min.js',
+                '/rozier-backend-theme-simple.3e530f8e.min.js',
             ),
         ),
         'custom-forms' => array(
             'css' => array(
-                '/vendor.8975c8e4.css',
+                '/vendor.6a0825ea.css',
                 '/custom-forms-front.667b8672.css',
             ),
             'js' => array(
-                '/rozier-backend-theme-cforms.fe9cc5ea.min.js',
+                '/rozier-backend-theme-cforms.f9395f47.min.js',
             ),
         ),
     )
