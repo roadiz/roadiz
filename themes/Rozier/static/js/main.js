110333e86b6d2420f0703c55a966ddc3b9435052
Ambroise Maupate <ambroise.maupate@gmail.com>
2015-03-27 11:29:05 +0100

    Added move to the first position methods

--- themes/Rozier/static/js/main.js
+++ themes/Rozier/static/js/main.js
@@ -484,6 +484,71 @@ Rozier.onNestableNodeTreeChange = function (event, element, status) {
 	});
 };
 
+/**
+ * Move a node to the first position.
+ *
+ * @param  Event event
+ * @param  jQueryNode element
+ * @param  string status  added, moved or removed
+ * @return boolean
+ */
+Rozier.moveNodeToFirstPosition = function (event, element, status) {
+	var _this = this;
+
+	console.log("Node: "+element.data('node-id')+ " status : "+status);
+
+	/*
+	 * If node removed, do not do anything, the othechange.uk.nestabler nodeTree will be triggered
+	 */
+	if (status == 'removed') {
+		return false;
+	}
+
+	var node_id = parseInt(element.data('node-id'));
+	var parent_node_id = parseInt(element.parents('ul').first().data('parent-node-id'));
+
+	var postData = {
+		_token: Rozier.ajaxToken,
+		_action: 'updatePosition',
+		nodeId: node_id
+	};
+
+	/*
+	 * Force to first position
+	 */
+	postData.nextNodeId = 0;
+
+	/*
+	 * When dropping to root
+	 * set parentNodeId to NULL
+	 */
+	if(isNaN(parent_node_id)){
+		parent_node_id = null;
+	}
+	postData.newParent = parent_node_id;
+
+	console.log(postData);
+	$.ajax({
+		url: Rozier.routes.nodeAjaxEdit.replace("%nodeId%", node_id),
+		type: 'POST',
+		dataType: 'json',
+		data: postData
+	})
+	.done(function( data ) {
+		console.log(data);
+		UIkit.notify({
+			message : data.responseText,
+			status  : data.status,
+			timeout : 3000,
+			pos     : 'top-center'
+		});
+
+	})
+	.fail(function( data ) {
+		console.log(data);
+	});
+};
+
 
 /**
  *
