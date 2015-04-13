5ee7e47954053ee7b77c657a2f0e447f85a82112
Maxime Bérard <maxime@rezo-zero.com>
2015-04-13 18:20:37 +0200

    Changed gsap filename

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -7,15 +7,15 @@ return array(
                 '/style.632eacd2.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.5256f6ee.min.js',
-                '/rozier-backend-theme-vendor.64c329b8.min.js',
+                '/rozier-backend-theme.2fcbd11c.min.js',
+                '/rozier-backend-theme-vendor.b8a5b7a2.min.js',
             ),
         ),
         'simple' => array(
             'css' => array(
             ),
             'js' => array(
-                '/rozier-backend-theme-simple.a01c45d1.min.js',
+                '/rozier-backend-theme-simple.3bb8c8be.min.js',
             ),
         ),
         'custom-forms' => array(
@@ -24,7 +24,7 @@ return array(
                 '/custom-forms-front.777ea3f7.css',
             ),
             'js' => array(
-                '/rozier-backend-theme-cforms.76e13524.min.js',
+                '/rozier-backend-theme-cforms.bc34952b.min.js',
             ),
         ),
     )
