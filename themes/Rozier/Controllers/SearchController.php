071cfa4990b49ac98398b805e0926945daa5a1cd
Ambroise Maupate <ambroise@rezo-zero.com>
2015-04-14 14:11:40 +0200

    Fixed wrong indent.

--- themes/Rozier/Controllers/SearchController.php
+++ themes/Rozier/Controllers/SearchController.php
@@ -321,18 +321,18 @@ class SearchController extends RozierApp
                                     ["method" => "get"]
                                 );
         $builderNodeType->add(
-                            "nodetype",
-                            new \RZ\Roadiz\CMS\Forms\NodeTypesType,
-                            [
-                                'empty_value' => "",
-                                'required' => false,
-                                'data' => $nodetypeId,
-                            ]
-                        )
-                        ->add("nodetypeSubmit", "submit", [
-                            "label" => $this->getTranslator()->trans("select.nodetype"),
-                            "attr" => ["class" => "uk-button uk-button-primary"],
-                        ]);
+            "nodetype",
+            new \RZ\Roadiz\CMS\Forms\NodeTypesType,
+            [
+                'empty_value' => "",
+                'required' => false,
+                'data' => $nodetypeId,
+            ]
+        )
+        ->add("nodetypeSubmit", "submit", [
+            "label" => $this->getTranslator()->trans("select.nodetype"),
+            "attr" => ["class" => "uk-button uk-button-primary"],
+        ]);
 
         return $builderNodeType;
     }
