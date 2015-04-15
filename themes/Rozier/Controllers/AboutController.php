44b39446f78c8439455ee1c3ee05ac5f879e8bec
Ambroise Maupate <ambroise@rezo-zero.com>
2015-04-15 11:20:06 +0200

    Added some routes and action to begin automatic Roadiz upgrade.

--- themes/Rozier/Controllers/AboutController.php
+++ themes/Rozier/Controllers/AboutController.php
@@ -32,14 +32,40 @@ namespace Themes\Rozier\Controllers;
 
 use GuzzleHttp\Subscriber\Cache\CacheStorage;
 use GuzzleHttp\Subscriber\Cache\CacheSubscriber;
+use GuzzleHttp\Stream\Stream;
+use GuzzleHttp\Client;
 use RZ\Roadiz\Core\Kernel;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Themes\Rozier\RozierApp;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 
 class AboutController extends RozierApp
 {
-    protected function getGithubLatestRelease()
+    const UPDATE_WITH_GIT = true;
+    /**
+     * Destination folder for updated files.
+     * When doing test set this as /testDir or something
+     * else than "/".
+     */
+    const UPDATE_DEST_DIR = "/testDir";
+
+    static $filesToUpload = [
+        'vendor',
+        'index.php',
+        'src',
+        'themes/Install',
+        'themes/Rozier',
+        'themes/DefaultTheme',
+        'bootstrap.php',
+        'bin',
+        'cli-config.php',
+        'tests',
+    ];
+
+    protected function getGithubReleases()
     {
         try {
             $url = "https://api.github.com/repos/roadiz/roadiz/releases";
@@ -64,28 +90,157 @@ class AboutController extends RozierApp
         }
     }
 
+    protected function canAutomaticUpdate()
+    {
+        $fs = new Filesystem();
+
+        if (!static::UPDATE_WITH_GIT && $fs->exists(ROADIZ_ROOT . '/.git')) {
+            throw new \Exception("cannot_update_roadiz.using_git", 1);
+        }
+        if (is_link(ROADIZ_ROOT . '/vendor') ||
+            is_link(ROADIZ_ROOT . '/src')) {
+            throw new \Exception("cannot_update_roadiz.using_symlink", 1);
+        }
+    }
+
+    protected function getLatestRelease()
+    {
+        $releases = $this->getGithubReleases();
+
+        if (false !== $releases) {
+            foreach ($releases as $release) {
+                if ($release->draft === false && $release->tag_name[0] == 'v') {
+                    return $release;
+                }
+            }
+        }
+
+        return null;
+    }
+
+
     public function indexAction(Request $request)
     {
         $this->validateAccessForRole('ROLE_SUPERADMIN');
 
-        $releases = $this->getGithubLatestRelease();
-        $lastRelease = null;
-        foreach ($releases as $release) {
-            $lastRelease = $release;
-            if ($release->draft === false && $lastRelease->tag_name[0] == 'v') {
-                break;
-            }
-        }
+        $lastRelease = $this->getLatestRelease();
+
         if ($lastRelease !== null) {
             $lastVersion = substr($lastRelease->tag_name, 1);
             $this->assignation["newVersion"] = version_compare(Kernel::$cmsVersion, $lastVersion, "<");
             if (isset($lastRelease->assets[0])) {
                 $this->assignation["downloadUrl"] = $lastRelease->assets[0]->browser_download_url;
             }
+            $this->assignation["lastRelease"] = $lastRelease;
             $this->assignation["changelog"] = $lastRelease->body;
             $this->assignation["currentVersion"] = Kernel::$cmsVersion;
             $this->assignation["lastVersion"] = $lastVersion;
         }
         return $this->render('about/index.html.twig', $this->assignation);
     }
+
+
+    public function updateAction(Request $request)
+    {
+        $this->validateAccessForRole('ROLE_SUPERADMIN');
+
+        try {
+            $this->assignation['errors'] = [];
+
+            $this->canAutomaticUpdate();
+
+            $this->assignation['nextStepRoute'] = $this->generateUrl('aboutUpdateDownloadPage');
+        } catch (\Exception $e) {
+            $this->publishErrorMessage($request, $e->getMessage());
+            $this->assignation['errors'][] = $e->getMessage();
+        }
+
+        return $this->render('about/update/index.html.twig', $this->assignation);
+    }
+
+    /**
+     * Download latest Roadiz release archive.
+     *
+     * @param  Request $request
+     *
+     * @return JsonResponse
+     */
+    public function downloadAction(Request $request)
+    {
+        $this->validateAccessForRole('ROLE_SUPERADMIN');
+
+        $lastRelease = $this->getLatestRelease();
+
+        if (null === $lastRelease) {
+            throw new \Exception("cannot_update_roadiz.no_release_available", 1);
+        }
+        if (!isset($lastRelease->assets[0]) || empty($lastRelease->assets[0]->browser_download_url)) {
+            throw new \Exception("cannot_update_roadiz.no_archive_to_download", 1);
+        }
+
+        $downloadUrl = $lastRelease->assets[0]->browser_download_url;
+
+
+        $tmpFile = tempnam(sys_get_temp_dir(), "roadiz_update.zip");
+        $resource = fopen($tmpFile, 'w');
+
+        $client = new Client();
+        $response = $client->get($downloadUrl, [
+            'body' => $resource
+        ]);
+
+        $request->getSession()->set('roadiz_update_archive', $tmpFile);
+
+        return new JsonResponse([
+            'tmpFile' => $tmpFile,
+            'nextStepRoute' => $this->generateUrl('aboutUpdateUnarchivePage')
+        ]);
+    }
+
+    /**
+     * Unarchive temporary roadiz archive.
+     *
+     * @param  Request $request
+     *
+     * @return JsonResponse
+     */
+    public function unarchiveAction(Request $request)
+    {
+        $this->validateAccessForRole('ROLE_SUPERADMIN');
+
+        if ($request->getSession()->get('roadiz_update_archive') !== "") {
+            $tmpFile = $request->getSession()->get('roadiz_update_archive');
+            $fs = new Filesystem();
+
+            if (!$fs->exists($tmpFile) || !is_readable($tmpFile)) {
+                throw new \Exception("cannot_update_roadiz.temp_archive_does_not_exist", 1);
+            }
+
+
+
+        } else {
+            throw new \Exception("cannot_update_roadiz.temp_archive_does_not_exist", 1);
+        }
+
+        return new JsonResponse([
+            'nextStepRoute' => $this->generateUrl('aboutUpdateMovePage')
+        ]);
+    }
+    public function moveFilesAction(Request $request)
+    {
+        $this->validateAccessForRole('ROLE_SUPERADMIN');
+
+        /*
+         * Define a files root that can be different than real root
+         * for testing purposes.
+         */
+        $rootPath = ROADIZ_ROOT . static::UPDATE_DEST_DIR;
+
+
+
+        return new JsonResponse([
+            'nextStepRoute' => $this->generateUrl('aboutUpdateMoveNewPage')
+        ]);
+    }
+
 }
--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -3,28 +3,28 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-                '/vendor.45ff5f6f.css',
+                '/vendor.c7a8e860.css',
                 '/style.632eacd2.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.2fcbd11c.min.js',
-                '/rozier-backend-theme-vendor.b8a5b7a2.min.js',
+                '/rozier-backend-theme.ea3f4794.min.js',
+                '/rozier-backend-theme-vendor.3ec9a063.min.js',
             ),
         ),
         'simple' => array(
             'css' => array(
             ),
             'js' => array(
-                '/rozier-backend-theme-simple.3bb8c8be.min.js',
+                '/rozier-backend-theme-simple.90128bee.min.js',
             ),
         ),
         'custom-forms' => array(
             'css' => array(
-                '/vendor.45ff5f6f.css',
+                '/vendor.c7a8e860.css',
                 '/custom-forms-front.777ea3f7.css',
             ),
             'js' => array(
-                '/rozier-backend-theme-cforms.bc34952b.min.js',
+                '/rozier-backend-theme-cforms.570cb686.min.js',
             ),
         ),
     )
