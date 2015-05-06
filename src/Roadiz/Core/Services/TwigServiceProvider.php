b37a5ab42290d49c365195edb63701784e6bcc71
Ambroise Maupate <ambroise@rezo-zero.com>
2015-05-06 15:43:43 +0200

    Switched twig custom filters to clean Extensions.

--- src/Roadiz/Core/Services/TwigServiceProvider.php
+++ src/Roadiz/Core/Services/TwigServiceProvider.php
@@ -33,13 +33,12 @@ use Asm89\Twig\CacheExtension\CacheProvider\DoctrineCacheAdapter;
 use Asm89\Twig\CacheExtension\CacheStrategy\LifetimeCacheStrategy;
 use Asm89\Twig\CacheExtension\Extension as CacheExtension;
 use Pimple\Container;
-use RZ\Roadiz\Core\AbstractEntities\AbstractEntity;
 use RZ\Roadiz\Core\Entities\Document;
-use RZ\Roadiz\Core\Entities\Node;
-use RZ\Roadiz\Core\Entities\NodesSources;
 use RZ\Roadiz\Core\Kernel;
 use RZ\Roadiz\Utils\TwigExtensions\BlockRenderExtension;
-use RZ\Roadiz\Utils\UrlGenerators\NodesSourcesUrlGenerator;
+use RZ\Roadiz\Utils\TwigExtensions\DocumentExtension;
+use RZ\Roadiz\Utils\TwigExtensions\NodesSourcesExtension;
+use RZ\Roadiz\Utils\TwigExtensions\UrlExtension;
 use Symfony\Bridge\Twig\Extension\FormExtension;
 use Symfony\Bridge\Twig\Extension\RoutingExtension;
 use Symfony\Bridge\Twig\Extension\TranslationExtension;
@@ -101,15 +100,6 @@ class TwigServiceProvider implements \Pimple\ServiceProviderInterface
             $twig->addFilter($c['twig.markdownExtension']);
             $twig->addFilter($c['twig.inlineMarkdownExtension']);
             $twig->addFilter($c['twig.centralTruncateExtension']);
-            $twig->addFilter($c['twig.displayExtension']);
-            $twig->addFilter($c['twig.urlExtension']);
-            $twig->addFilter($c['twig.childrenExtension']);
-            $twig->addFilter($c['twig.nextExtension']);
-            $twig->addFilter($c['twig.previousExtension']);
-            $twig->addFilter($c['twig.lastSibling']);
-            $twig->addFilter($c['twig.firstSibling']);
-            $twig->addFilter($c['twig.parent']);
-            $twig->addFilter($c['twig.parents']);
 
             /*
              * Extensions
@@ -119,6 +109,9 @@ class TwigServiceProvider implements \Pimple\ServiceProviderInterface
             $twig->addExtension($c['twig.routingExtension']);
             $twig->addExtension(new \Twig_Extensions_Extension_Text());
             $twig->addExtension(new BlockRenderExtension($c, Kernel::getInstance()));
+            $twig->addExtension(new NodesSourcesExtension($c['securityContext']));
+            $twig->addExtension(new DocumentExtension());
+            $twig->addExtension(new UrlExtension($c['request']));
 
             if (null !== $c['twig.cacheExtension']) {
                 $twig->addExtension($c['twig.cacheExtension']);
@@ -147,82 +140,6 @@ class TwigServiceProvider implements \Pimple\ServiceProviderInterface
         $container['twig.routingExtension'] = function ($c) {
 
             return new RoutingExtension($c['urlGenerator']);
-        };
-
-        /*
-         * Document extensions
-         */
-        $container['twig.displayExtension'] = function ($c) {
-            return new \Twig_SimpleFilter('display', function (Document $document, array $criteria = []) {
-                return $document->getViewer()->getDocumentByArray($criteria);
-            }, ['is_safe' => ['html']]);
-        };
-        $container['twig.urlExtension'] = function ($c) {
-            return new \Twig_SimpleFilter('url', function (AbstractEntity $mixed, array $criteria = []) {
-
-                if ($mixed instanceof Document) {
-                    return $mixed->getViewer()->getDocumentUrlByArray($criteria);
-                } elseif ($mixed instanceof NodesSources) {
-                    $urlGenerator = new NodesSourcesUrlGenerator(
-                        Kernel::getService('request'),
-                        $mixed
-                    );
-                    if (isset($criteria['absolute'])) {
-                        return $urlGenerator->getUrl((boolean) $criteria['absolute']);
-                    }
-                    return $urlGenerator->getUrl(false);
-                } elseif ($mixed instanceof Node) {
-                    $urlGenerator = new NodesSourcesUrlGenerator(
-                        Kernel::getService('request'),
-                        $mixed->getNodeSources()->first()
-                    );
-                    if (isset($criteria['absolute'])) {
-                        return $urlGenerator->getUrl((boolean) $criteria['absolute']);
-                    }
-                    return $urlGenerator->getUrl(false);
-                } else {
-                    throw new \RuntimeException("Twig “url” filter can be only used with a Document, a NodesSources or a Node", 1);
-                }
-            });
-        };
-        /*
-         * NodesSources extensions
-         */
-        $container['twig.childrenExtension'] = function ($c) {
-            return new \Twig_SimpleFilter('children', function (NodesSources $ns, array $criteria = null, array $order = null) {
-                return $ns->getHandler()->getChildren($criteria, $order, Kernel::getService('securityContext'));
-            });
-        };
-        $container['twig.nextExtension'] = function ($c) {
-            return new \Twig_SimpleFilter('next', function (NodesSources $ns, array $criteria = null, array $order = null) {
-                return $ns->getHandler()->getNext($criteria, $order, Kernel::getService('securityContext'));
-            });
-        };
-        $container['twig.previousExtension'] = function ($c) {
-            return new \Twig_SimpleFilter('previous', function (NodesSources $ns, array $criteria = null, array $order = null) {
-                return $ns->getHandler()->getPrevious($criteria, $order, Kernel::getService('securityContext'));
-            });
-        };
-        $container['twig.lastSibling'] = function ($c) {
-            return new \Twig_SimpleFilter('lastSibling', function (NodesSources $ns, array $criteria = null, array $order = null) {
-                return $ns->getHandler()->getLastSibling($criteria, $order, Kernel::getService('securityContext'));
-            });
-        };
-        $container['twig.firstSibling'] = function ($c) {
-            return new \Twig_SimpleFilter('firstSibling', function (NodesSources $ns, array $criteria = null, array $order = null) {
-                return $ns->getHandler()->getFirstSibling($criteria, $order, Kernel::getService('securityContext'));
-            });
-        };
-        $container['twig.parent'] = function ($c) {
-            return new \Twig_SimpleFilter('parent', function (NodesSources $ns) {
-                return $ns->getHandler()->getParent();
-            });
-        };
-
-        $container['twig.parents'] = function ($c) {
-            return new \Twig_SimpleFilter('parents', function (NodesSources $ns, array $criteria = []) {
-                return $ns->getHandler()->getParents($criteria, Kernel::getService('securityContext'));
-            });
         };
 
         /*
