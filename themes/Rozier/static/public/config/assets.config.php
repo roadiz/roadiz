784a5f9d66b143bef977422f68622855d3979998
Maxime Bérard <maxime@rezo-zero.com>
2015-05-06 16:52:10 +0200

    Fixed #139 : Impossible to search documents in documents explorer (padding problem)

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -4,18 +4,18 @@ return array(
         'global' => array(
             'css' => array(
                 '/vendor.dd6b4244.css',
-                '/style.b5290685.css',
+                '/style.b0c2eff8.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.41290db6.min.js',
-                '/rozier-backend-theme-vendor.29353d0e.min.js',
+                '/rozier-backend-theme.101fc567.min.js',
+                '/rozier-backend-theme-vendor.ca7be1e0.min.js',
             ),
         ),
         'simple' => array(
             'css' => array(
             ),
             'js' => array(
-                '/rozier-backend-theme-simple.12429856.min.js',
+                '/rozier-backend-theme-simple.73aed3f6.min.js',
             ),
         ),
         'custom-forms' => array(
@@ -24,7 +24,7 @@ return array(
                 '/custom-forms-front.0b334c0e.css',
             ),
             'js' => array(
-                '/rozier-backend-theme-cforms.33b4177c.min.js',
+                '/rozier-backend-theme-cforms.4bd958f9.min.js',
             ),
         ),
     )
