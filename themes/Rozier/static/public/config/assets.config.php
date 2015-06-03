98ace248acbe43a0eb2c84da51c8014a5e0f31a7
Ambroise Maupate <ambroise.maupate@gmail.com>
2015-06-03 02:12:57 +0200

    Added a tooltip on document thumbnail to display entire filename. #150

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -4,27 +4,27 @@ return array(
         'global' => array(
             'css' => array(
                 '/vendor.58ec8843.css',
-                '/style.3f476087.css',
+                '/style.ae989b58.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.3993541e.min.js',
-                '/rozier-backend-theme-vendor.933e5e2f.min.js',
+                '/rozier-backend-theme.5aed58eb.min.js',
+                '/rozier-backend-theme-vendor.633dd9d5.min.js',
             ),
         ),
         'simple' => array(
             'css' => array(
             ),
             'js' => array(
-                '/rozier-backend-theme-simple.95accfed.min.js',
+                '/rozier-backend-theme-simple.abfeb801.min.js',
             ),
         ),
         'custom-forms' => array(
             'css' => array(
                 '/vendor.58ec8843.css',
-                '/custom-forms-front.e22c4698.css',
+                '/custom-forms-front.eb7e161f.css',
             ),
             'js' => array(
-                '/rozier-backend-theme-cforms.4691e5b8.min.js',
+                '/rozier-backend-theme-cforms.faa6db92.min.js',
             ),
         ),
     )
