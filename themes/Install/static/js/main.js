b1fe7f189578de74f2982ab0e872b86556aabff7
Ambroise Maupate <ambroise@rezo-zero.com>
2015-06-26 11:11:42 +0200

    Uniformized import JS vars.

--- themes/Install/static/js/main.js
+++ themes/Install/static/js/main.js
@@ -4,7 +4,6 @@
  * ============================================================================
  */
 var Install = {
-    importFixtures: null,
     selectDatabaseField: null,
     resizeContainer: null,
     import: null
@@ -14,16 +13,12 @@ Install.onDocumentReady = function( event ) {
 
     Install.resizeContainer = new resizeContainer();
 
-    if(typeof Install.importRoutes != "undefined"){
-        Install.importFixtures = new ImportFixtures(Install.importRoutes);
-    }
-
     if ($("#databaseForm").length) {
         Install.selectDatabaseField = new SelectDatabaseField();
     }
 
-    if (typeof Install.importThemeRoutes != "undefined"){
-        Install.import = new Import(Install.importThemeRoutes);
+    if (typeof Install.importRoutes != "undefined"){
+        Install.import = new Import(Install.importRoutes);
     }
 
     // Add boostrap switch to checkbox
