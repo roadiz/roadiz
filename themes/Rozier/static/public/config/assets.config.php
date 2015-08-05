45969dcae81e047992caf0823e5fa4aabc8e639e
Ambroise Maupate <ambroise@rezo-zero.com>
2015-08-05 20:04:13 +0200

    Added unpublished home icon.

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -4,18 +4,18 @@ return array(
         'global' => array(
             'css' => array(
                 '/vendor.1546beea.css',
-                '/style.78478d12.css',
+                '/style.62604a38.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.0f1acf16.min.js',
-                '/rozier-backend-theme-vendor.117f54e4.min.js',
+                '/rozier-backend-theme.689c9ccc.min.js',
+                '/rozier-backend-theme-vendor.62bf2036.min.js',
             ),
         ),
         'simple' => array(
             'css' => array(
             ),
             'js' => array(
-                '/rozier-backend-theme-simple.46eaf3e9.min.js',
+                '/rozier-backend-theme-simple.91b7b241.min.js',
             ),
         ),
         'custom-forms' => array(
@@ -24,7 +24,7 @@ return array(
                 '/custom-forms-front.47be0471.css',
             ),
             'js' => array(
-                '/rozier-backend-theme-cforms.97372762.min.js',
+                '/rozier-backend-theme-cforms.7c012ee6.min.js',
             ),
         ),
     )
