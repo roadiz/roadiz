b5260046aee7e101ac6dbe96ee40842fa11ed9b4
Ambroise Maupate <ambroise@rezo-zero.com>
2015-09-11 12:25:13 +0200

    Fixed delete button on locked nodes.

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -4,27 +4,27 @@ return array(
         'global' => array(
             'css' => array(
                 '/vendor.1546beea.css',
-                '/style.69f54276.css',
+                '/style.bda37338.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.770aef19.min.js',
-                '/rozier-backend-theme-vendor.490f52d4.min.js',
+                '/rozier-backend-theme.076838b7.min.js',
+                '/rozier-backend-theme-vendor.ac651c81.min.js',
             ),
         ),
         'simple' => array(
             'css' => array(
             ),
             'js' => array(
-                '/rozier-backend-theme-simple.a8bddd9e.min.js',
+                '/rozier-backend-theme-simple.35ba2cc6.min.js',
             ),
         ),
         'custom-forms' => array(
             'css' => array(
                 '/vendor.1546beea.css',
-                '/custom-forms-front.47be0471.css',
+                '/custom-forms-front.664933d7.css',
             ),
             'js' => array(
-                '/rozier-backend-theme-cforms.9e8744af.min.js',
+                '/rozier-backend-theme-cforms.d1a6c53b.min.js',
             ),
         ),
     )
