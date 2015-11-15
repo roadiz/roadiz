7d2f4a570d41fd8bc5c6f5c69b545e12a39701c0
Ambroise Maupate <ambroise.maupate@gmail.com>
2015-11-15 22:57:39 +0100

    Some FR and EN translations.

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -4,18 +4,18 @@ return array(
         'global' => array(
             'css' => array(
                 '/vendor.636407e2.css',
-                '/style.f4f1c4bf.css',
+                '/style.88f54171.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.0c015a4d.min.js',
-                '/rozier-backend-theme-vendor.1d220b3a.min.js',
+                '/rozier-backend-theme.84fef79b.min.js',
+                '/rozier-backend-theme-vendor.cdaab013.min.js',
             ),
         ),
         'simple' => array(
             'css' => array(
             ),
             'js' => array(
-                '/rozier-backend-theme-simple.e6b46e8c.min.js',
+                '/rozier-backend-theme-simple.4ed13fa8.min.js',
             ),
         ),
         'custom-forms' => array(
@@ -24,7 +24,7 @@ return array(
                 '/custom-forms-front.9abcc3d5.css',
             ),
             'js' => array(
-                '/rozier-backend-theme-cforms.d74b76b7.min.js',
+                '/rozier-backend-theme-cforms.c8b7b735.min.js',
             ),
         ),
     )
