cfbf43dc7961356f6b40a1b3ac1698e793553b75
Ambroise Maupate <ambroise.maupate@gmail.com>
2015-12-06 13:08:33 +0100

    Fixed object style on login page.

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -4,18 +4,18 @@ return array(
         'global' => array(
             'css' => array(
                 '/vendor.636407e2.css',
-                '/style.a4717f2f.css',
+                '/style.8776dccf.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.b3d35402.min.js',
-                '/rozier-backend-theme-vendor.804758ad.min.js',
+                '/rozier-backend-theme.8bae4a13.min.js',
+                '/rozier-backend-theme-vendor.8b815499.min.js',
             ),
         ),
         'simple' => array(
             'css' => array(
             ),
             'js' => array(
-                '/rozier-backend-theme-simple.9ba226fd.min.js',
+                '/rozier-backend-theme-simple.f9765e3d.min.js',
             ),
         ),
         'custom-forms' => array(
@@ -24,7 +24,7 @@ return array(
                 '/custom-forms-front.fd8064b1.css',
             ),
             'js' => array(
-                '/rozier-backend-theme-cforms.6e6df8a8.min.js',
+                '/rozier-backend-theme-cforms.d016a11c.min.js',
             ),
         ),
     )
