b51eb640589c10cc6ebb1f27f5f2726dfb0f95e7
Ambroise Maupate <ambroise@rezo-zero.com>
2016-01-07 20:08:00 +0100

    Remove border-top on first node in children node widget.

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -4,18 +4,18 @@ return array(
         'global' => array(
             'css' => array(
                 '/vendor.636407e2.css',
-                '/style.17594663.css',
+                '/style.8e692811.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.a9c7e377.min.js',
-                '/rozier-backend-theme-vendor.2db8aac6.min.js',
+                '/rozier-backend-theme.9a1d2409.min.js',
+                '/rozier-backend-theme-vendor.3d45bd4c.min.js',
             ),
         ),
         'simple' => array(
             'css' => array(
             ),
             'js' => array(
-                '/rozier-backend-theme-simple.ad7fc98f.min.js',
+                '/rozier-backend-theme-simple.ffe49d46.min.js',
             ),
         ),
         'custom-forms' => array(
@@ -24,7 +24,7 @@ return array(
                 '/custom-forms-front.fd8064b1.css',
             ),
             'js' => array(
-                '/rozier-backend-theme-cforms.5bc59ba1.min.js',
+                '/rozier-backend-theme-cforms.b94b8e1d.min.js',
             ),
         ),
     )
