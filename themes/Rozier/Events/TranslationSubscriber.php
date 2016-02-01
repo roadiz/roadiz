cf4110f711b1f644616d00fd2d06cfcaee75384d
Ambroise Maupate <ambroise@rezo-zero.com>
2016-02-01 09:50:04 +0100

    Added translation subscriber to empty cache.

---
+++ themes/Rozier/Events/TranslationSubscriber.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Copyright © 2016, Ambroise Maupate and Julien Blanchet
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * Except as contained in this notice, the name of the ROADIZ shall not
+ * be used in advertising or otherwise to promote the sale, use or other dealings
+ * in this Software without prior written authorization from Ambroise Maupate and Julien Blanchet.
+ *
+ * @file TranslationSubscriber.php
+ * @author Ambroise Maupate
+ */
+namespace Themes\Rozier\Events;
+
+use Doctrine\Common\Cache\CacheProvider;
+use RZ\Roadiz\Core\Events\TranslationEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Subscribe to Translation event to clear result cache.
+ */
+class TranslationSubscriber implements EventSubscriberInterface
+{
+    protected $cacheProvider = null;
+
+    public function __construct(CacheProvider $cacheProvider)
+    {
+        $this->cacheProvider = $cacheProvider;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            TranslationEvents::TRANSLATION_CREATED => 'purgeCache',
+            TranslationEvents::TRANSLATION_UPDATED => 'purgeCache',
+            TranslationEvents::TRANSLATION_DELETED => 'purgeCache',
+        ];
+    }
+
+    /**
+     * Empty nodeSources Url cache
+     */
+    public function purgeCache()
+    {
+        $this->cacheProvider->deleteAll();
+    }
+}
