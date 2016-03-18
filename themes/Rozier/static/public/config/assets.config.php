915ab397030646856c17448c61486d2783447548
Ambroise Maupate <ambroise@rezo-zero.com>
2016-03-18 18:46:18 +0100

    Added some style for Money input fields.

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -4,27 +4,27 @@ return array(
         'global' => array(
             'css' => array(
                 '/vendor.636407e2.css',
-                '/style.8e692811.css',
+                '/style.d846b953.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.11b89fdd.min.js',
-                '/rozier-backend-theme-vendor.590ee4b1.min.js',
+                '/rozier-backend-theme.8f331b5e.min.js',
+                '/rozier-backend-theme-vendor.606d26c9.min.js',
             ),
         ),
         'simple' => array(
             'css' => array(
             ),
             'js' => array(
-                '/rozier-backend-theme-simple.7a04ee05.min.js',
+                '/rozier-backend-theme-simple.adb6185b.min.js',
             ),
         ),
         'custom-forms' => array(
             'css' => array(
                 '/vendor.636407e2.css',
-                '/custom-forms-front.fd8064b1.css',
+                '/custom-forms-front.c92afbad.css',
             ),
             'js' => array(
-                '/rozier-backend-theme-cforms.1cbbc4c3.min.js',
+                '/rozier-backend-theme-cforms.f18a561a.min.js',
             ),
         ),
     )
