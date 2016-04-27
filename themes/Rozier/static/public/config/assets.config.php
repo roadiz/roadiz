5ebc69f588f51ab40053b95a0165d34eebbd2e50
Ambroise Maupate <ambroise@rezo-zero.com>
2016-04-27 10:55:42 +0200

    Fixed broken children and stack for tags and folder too.

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -7,15 +7,15 @@ return array(
                 '/style.dd410f13.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.84320214.min.js',
-                '/rozier-backend-theme-vendor.43a05fa6.min.js',
+                '/rozier-backend-theme.8988250f.min.js',
+                '/rozier-backend-theme-vendor.d7ddcc68.min.js',
             ),
         ),
         'simple' => array(
             'css' => array(
             ),
             'js' => array(
-                '/rozier-backend-theme-simple.86c95f79.min.js',
+                '/rozier-backend-theme-simple.4b959aeb.min.js',
             ),
         ),
         'custom-forms' => array(
@@ -24,7 +24,7 @@ return array(
                 '/custom-forms-front.724bdd55.css',
             ),
             'js' => array(
-                '/rozier-backend-theme-cforms.ce781489.min.js',
+                '/rozier-backend-theme-cforms.3971287c.min.js',
             ),
         ),
     )
