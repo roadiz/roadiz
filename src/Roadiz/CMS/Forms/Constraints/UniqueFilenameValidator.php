bc17c07033fb7841614eec87c49ebc2c08c6badd
Ambroise Maupate <ambroise@rezo-zero.com>
2016-06-02 20:42:39 +0200

    Fixes document renaming when no extension is used or if filename already exists.

---
+++ src/Roadiz/CMS/Forms/Constraints/UniqueFilenameValidator.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Copyright (c) 2016. Ambroise Maupate and Julien Blanchet
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * Except as contained in this notice, the name of the ROADIZ shall not
+ * be used in advertising or otherwise to promote the sale, use or other dealings
+ * in this Software without prior written authorization from Ambroise Maupate and Julien Blanchet.
+ *
+ * @file UniqueFilenameValidator.php
+ * @author ambroisemaupate
+ */
+
+namespace RZ\Roadiz\CMS\Forms\Constraints;
+
+use RZ\Roadiz\Core\Entities\Document;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+/**
+ * Class UniqueFilenameValidator
+ * @package RZ\Roadiz\CMS\Forms\Constraints
+ */
+class UniqueFilenameValidator extends ConstraintValidator
+{
+    /**
+     * @param mixed $value
+     * @param Constraint $constraint
+     */
+    public function validate($value, Constraint $constraint)
+    {
+        /** @var Document $document */
+        $document = $constraint->document;
+        /*
+         * If value is already the node name
+         * do nothing.
+         */
+        if (null !== $document &&
+            $value == $document->getFilename()) {
+            return;
+        }
+
+        $fs = new Filesystem();
+        $folder = dirname($document->getAbsolutePath());
+        if ($fs->exists($folder . '/' . $value)) {
+            $this->context->addViolation($constraint->message);
+        }
+    }
+}
