b5354624c7b65f36540ba2644b9938b34010e6b8
Ambroise Maupate <ambroise@rezo-zero.com>
2016-06-22 10:22:53 +0200

    Improved backoffice layout fr 13" screens.

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -4,27 +4,27 @@ return array(
         'global' => array(
             'css' => array(
                 '/vendor.636407e2.css',
-                '/style.6c288054.css',
+                '/style.b1cc56b0.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.5f205b0f.min.js',
-                '/rozier-backend-theme-vendor.71c24a90.min.js',
+                '/rozier-backend-theme.6e5d6636.min.js',
+                '/rozier-backend-theme-vendor.e9e50260.min.js',
             ),
         ),
         'simple' => array(
             'css' => array(
             ),
             'js' => array(
-                '/rozier-backend-theme-simple.d10b28be.min.js',
+                '/rozier-backend-theme-simple.c02b8e8f.min.js',
             ),
         ),
         'custom-forms' => array(
             'css' => array(
                 '/vendor.636407e2.css',
-                '/custom-forms-front.724bdd55.css',
+                '/custom-forms-front.b43c3824.css',
             ),
             'js' => array(
-                '/rozier-backend-theme-cforms.1ba19cf4.min.js',
+                '/rozier-backend-theme-cforms.3a0a5d4e.min.js',
             ),
         ),
     )
