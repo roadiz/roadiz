3123b419a02666a9dcfdff12b87e0294cfae1606
Ambroise Maupate <ambroise@rezo-zero.com>
2016-07-11 16:19:56 +0200

    Upgrade Vagrant solr provisioning.

--- samples/vagrant-solr-provisioning.sh
+++ samples/vagrant-solr-provisioning.sh
@@ -3,23 +3,28 @@
 export DEBIAN_FRONTEND=noninteractive
 
 # Apache Solr
-SOLR_VERSION="5.3.1"
-SOLR_MIRROR="http://apache.mirrors.ovh.net/ftp.apache.org/dist"
+SOLR_VERSION="6.1.0"
+SOLR_MIRROR="http://www-eu.apache.org/dist"
 
 sudo apt-get -qq update;
 
-# Install Apache Solr - based on article from Tomasz Muras - https://twitter.com/zabuch
-# http://jmuras.com/blog/2012/setup-solr-4-tomcat-ubuntu-server-12-04-lts/
-echo -e "\n--- Installing Open JDK ---\n"
-sudo apt-get -qq -f -y install openjdk-7-jre-headless unzip > /dev/null 2>&1;
+echo -e "\n--- Installing Oracle JDK 8 ---\n"
+sudo apt-get install -qq -f -y python-software-properties debconf-utils unzip > /dev/null 2>&1;
+sudo add-apt-repository -y ppa:webupd8team/java > /dev/null 2>&1;
+sudo apt-get -qq update > /dev/null 2>&1;
+# Accept silently Oracle license
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections > /dev/null 2>&1;
+sudo apt-get install -qq -f -y oracle-java8-installer > /dev/null 2>&1;
 
-echo -e "\n--- Installing Apache Solr (may take a while, be patient) ---\n"
-cd /tmp/
+echo -e "\n--- Downloading Apache Solr (may take a while, be patient) ---\n"
 sudo wget –q --output-document=solr-$SOLR_VERSION.tgz $SOLR_MIRROR/lucene/solr/$SOLR_VERSION/solr-$SOLR_VERSION.tgz > /dev/null 2>&1
-tar xzf solr-$SOLR_VERSION.tgz
+echo -e "\n--- Installing Apache Solr (may take a while, be patient) ---\n"
+sudo tar xzf solr-$SOLR_VERSION.tgz
 sudo cp -fr solr-$SOLR_VERSION /opt/solr
-sudo cp /opt/solr/bin/init.d/solr /etc/init.d/solr
-sudo sed -i "s/RUNAS=solr/#RUNAS=solr/" /etc/init.d/solr
+sudo ln -s /opt/solr/bin/solr.in.sh /etc/default/solr.in.sh
+sudo sed -i "s/RUNAS=\"solr\"/#RUNAS=\"root\"/" /opt/solr/bin/init.d/solr
+sudo ln -s /opt/solr/bin/init.d/solr /etc/init.d/solr
+sudo mkdir -p /opt/solr/server/logs
 sudo mkdir -p /var/solr
 sudo cp /opt/solr/bin/solr.in.sh /var/solr/solr.in.sh
 sudo update-rc.d solr defaults > /dev/null 2>&1;
@@ -31,10 +36,6 @@ sudo /opt/solr/bin/solr create_core -c roadiz > /dev/null 2>&1;
 
 echo -e "\n--- Restarting Solr server ---\n"
 sudo service solr restart > /dev/null 2>&1;
-
-##### CLEAN UP #####
-sudo dpkg --configure -a  > /dev/null 2>&1; # when upgrade or install doesnt run well (e.g. loss of connection) this may resolve quite a few issues
-sudo apt-get autoremove -y  > /dev/null 2>&1; # remove obsolete packages
 
 echo -e "\n-----------------------------------------------------------"
 echo -e "\n---------------- Your Solr server is ready ----------------"
