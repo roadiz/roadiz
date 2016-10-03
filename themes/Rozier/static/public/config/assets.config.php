30a65fb410e93f76cfa597a7cc25dc57b8245723
Ambroise Maupate <ambroise@rezo-zero.com>
2016-10-03 14:44:07 +0200

    Improved Children node widget layout.

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -4,27 +4,27 @@ return array(
         'global' => array(
             'css' => array(
                 '/vendor.9ba3ce6f.css',
-                '/style.e7871282.css',
+                '/style.734e1d3d.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.eb753370.min.js',
-                '/rozier-backend-theme-vendor.feb1f877.min.js',
+                '/rozier-backend-theme.cf337a5a.min.js',
+                '/rozier-backend-theme-vendor.3b69b5d7.min.js',
             ),
         ),
         'simple' => array(
             'css' => array(
             ),
             'js' => array(
-                '/rozier-backend-theme-simple.6e8f2689.min.js',
+                '/rozier-backend-theme-simple.77d1205d.min.js',
             ),
         ),
         'custom-forms' => array(
             'css' => array(
                 '/vendor.9ba3ce6f.css',
-                '/custom-forms-front.c554b62c.css',
+                '/custom-forms-front.c860e455.css',
             ),
             'js' => array(
-                '/rozier-backend-theme-cforms.8238e819.min.js',
+                '/rozier-backend-theme-cforms.a91136fe.min.js',
             ),
         ),
     )
