1f744e3ec675eac7b59d2e40b1e6aa531e3bf083
Ambroise Maupate <ambroise@rezo-zero.com>
2016-10-20 11:50:22 +0200

    Fixed children widget contextual menu.

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -4,27 +4,27 @@ return array(
         'global' => array(
             'css' => array(
                 '/vendor.9ba3ce6f.css',
-                '/style.babafde4.css',
+                '/style.d188f8da.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.57bf90ce.min.js',
-                '/rozier-backend-theme-vendor.e5f4b06c.min.js',
+                '/rozier-backend-theme.9f2ebecf.min.js',
+                '/rozier-backend-theme-vendor.334669dc.min.js',
             ),
         ),
         'simple' => array(
             'css' => array(
             ),
             'js' => array(
-                '/rozier-backend-theme-simple.10efa64c.min.js',
+                '/rozier-backend-theme-simple.57843a27.min.js',
             ),
         ),
         'custom-forms' => array(
             'css' => array(
                 '/vendor.9ba3ce6f.css',
-                '/custom-forms-front.43c53306.css',
+                '/custom-forms-front.ca6f9150.css',
             ),
             'js' => array(
-                '/rozier-backend-theme-cforms.8704b9dc.min.js',
+                '/rozier-backend-theme-cforms.a5be368a.min.js',
             ),
         ),
     )
