22d3149e70c956bdeb4f1dc26fe20374ee514a45
Ambroise Maupate <ambroise@rezo-zero.com>
2016-11-09 11:25:47 +0100

    Improved node-explorer layout.

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -4,18 +4,18 @@ return array(
         'global' => array(
             'css' => array(
                 '/vendor.9ba3ce6f.css',
-                '/style.8d7f0e3b.css',
+                '/style.1ec80756.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.75f76c52.min.js',
-                '/rozier-backend-theme-vendor.c5582b31.min.js',
+                '/rozier-backend-theme.55e24861.min.js',
+                '/rozier-backend-theme-vendor.76c4ff30.min.js',
             ),
         ),
         'simple' => array(
             'css' => array(
             ),
             'js' => array(
-                '/rozier-backend-theme-simple.93ac98ab.min.js',
+                '/rozier-backend-theme-simple.a26101c9.min.js',
             ),
         ),
         'custom-forms' => array(
@@ -24,7 +24,7 @@ return array(
                 '/custom-forms-front.ca6f9150.css',
             ),
             'js' => array(
-                '/rozier-backend-theme-cforms.828216ec.min.js',
+                '/rozier-backend-theme-cforms.aaeb65ac.min.js',
             ),
         ),
     )
