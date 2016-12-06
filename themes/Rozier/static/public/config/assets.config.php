9afbcfe785f4c13ec9089c0d6e831b7a3d1286e0
Ambroise Maupate <ambroise@rezo-zero.com>
2016-12-06 23:01:35 +0100

    Improved button sizes for small desktops and fixed user images scale.

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -4,27 +4,27 @@ return array(
         'global' => array(
             'css' => array(
                 '/vendor.9ba3ce6f.css',
-                '/style.849adc59.css',
+                '/style.0be5ca95.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.c0c14806.min.js',
-                '/rozier-backend-theme-vendor.a805f4a7.min.js',
+                '/rozier-backend-theme.c5e28a6d.min.js',
+                '/rozier-backend-theme-vendor.da420036.min.js',
             ),
         ),
         'simple' => array(
             'css' => array(
             ),
             'js' => array(
-                '/rozier-backend-theme-simple.4e1d0840.min.js',
+                '/rozier-backend-theme-simple.b04c836a.min.js',
             ),
         ),
         'custom-forms' => array(
             'css' => array(
                 '/vendor.9ba3ce6f.css',
-                '/custom-forms-front.d7c00f6d.css',
+                '/custom-forms-front.71d73a15.css',
             ),
             'js' => array(
-                '/rozier-backend-theme-cforms.443a070a.min.js',
+                '/rozier-backend-theme-cforms.dae8585c.min.js',
             ),
         ),
     )
