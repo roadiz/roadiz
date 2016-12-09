077d7a055d3c9c7e4e8f4c14d710c17a63025e3d
Ambroise Maupate <ambroise@rezo-zero.com>
2016-12-09 19:56:57 +0100

    Improved XS CSS styles.

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -4,18 +4,18 @@ return array(
         'global' => array(
             'css' => array(
                 '/vendor.9ba3ce6f.css',
-                '/style.2ec9ac0f.css',
+                '/style.b1fb397f.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.2bb3fc46.min.js',
-                '/rozier-backend-theme-vendor.c39a7d68.min.js',
+                '/rozier-backend-theme.97a2dbd7.min.js',
+                '/rozier-backend-theme-vendor.81a3ddfc.min.js',
             ),
         ),
         'simple' => array(
             'css' => array(
             ),
             'js' => array(
-                '/rozier-backend-theme-simple.dad7cc74.min.js',
+                '/rozier-backend-theme-simple.9b67d099.min.js',
             ),
         ),
         'custom-forms' => array(
@@ -24,7 +24,7 @@ return array(
                 '/custom-forms-front.6cd8f4b1.css',
             ),
             'js' => array(
-                '/rozier-backend-theme-cforms.b36f5268.min.js',
+                '/rozier-backend-theme-cforms.79dbc7e1.min.js',
             ),
         ),
     )
