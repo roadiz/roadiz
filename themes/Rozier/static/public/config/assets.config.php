73482f4855af797dece7e2b2f1b4ac4260bfade9
Ambroise Maupate <ambroise@rezo-zero.com>
2017-02-24 17:38:39 +0100

    Fixed non-nestable node tree layout and Vagrant PHP pool group.

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -4,18 +4,18 @@ return array(
         'global' => array(
             'css' => array(
                 '/vendor.303ca180.css',
-                '/style.cb43a282.css',
+                '/style.d8b78b31.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.fa8a0adc.min.js',
-                '/rozier-backend-theme-vendor.a9679771.min.js',
+                '/rozier-backend-theme.8d90f1dd.min.js',
+                '/rozier-backend-theme-vendor.f555d8c1.min.js',
             ),
         ),
         'simple' => array(
             'css' => array(
             ),
             'js' => array(
-                '/rozier-backend-theme-simple.c40d3168.min.js',
+                '/rozier-backend-theme-simple.deb2f6fd.min.js',
             ),
         ),
         'custom-forms' => array(
@@ -24,7 +24,7 @@ return array(
                 '/custom-forms-front.cd47dba9.css',
             ),
             'js' => array(
-                '/rozier-backend-theme-cforms.60612942.min.js',
+                '/rozier-backend-theme-cforms.c0b65ddf.min.js',
             ),
         ),
     )
