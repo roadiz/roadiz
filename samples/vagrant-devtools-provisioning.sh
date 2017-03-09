1a0e193a9613c52e759dea7e5454d3c0ac3fa7db
Ambroise Maupate <ambroise@rezo-zero.com>
2017-03-09 19:19:56 +0100

    FIxed composer install programmatically.

--- samples/vagrant-devtools-provisioning.sh
+++ samples/vagrant-devtools-provisioning.sh
@@ -9,10 +9,18 @@ echo -e "\n--- Installing NodeJS and NPM ---\n"
 sudo apt-get -y install nodejs > /dev/null 2>&1
 
 echo -e "\n--- Installing Composer for PHP package management ---\n"
+EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '55d6ead61b29c7bdee5cccfb50076874187bd9f21f65d8991d46ec5cc90518f447387fb9f76ebae1fbbacf329e583e30') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
+ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+then
+    >&2 echo 'ERROR: Invalid installer signature'
+    rm composer-setup.php
+    exit 1
+fi
+php composer-setup.php --quiet
+RESULT=$?
+rm composer-setup.php
 
 sudo mv composer.phar /usr/local/bin/composer
 
