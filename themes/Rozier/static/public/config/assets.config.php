4f2f31b163e72248f823049947a29984593af280
Ambroise Maupate <ambroise@rezo-zero.com>
2017-03-16 15:36:09 +0100

    Improved node-trees CSS with publishedAt field.

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -3,28 +3,28 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-                '/vendor.5c6b2574.css',
-                '/style.d1e95dd7.css',
+                '/vendor.303ca180.css',
+                '/style.a813c248.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.22674af3.min.js',
-                '/rozier-backend-theme-vendor.1946ae92.min.js',
+                '/rozier-backend-theme.6199439a.min.js',
+                '/rozier-backend-theme-vendor.59f034fb.min.js',
             ),
         ),
         'simple' => array(
             'css' => array(
             ),
             'js' => array(
-                '/rozier-backend-theme-simple.538aee7b.min.js',
+                '/rozier-backend-theme-simple.b5fa2685.min.js',
             ),
         ),
         'custom-forms' => array(
             'css' => array(
-                '/vendor.5c6b2574.css',
+                '/vendor.303ca180.css',
                 '/custom-forms-front.55adca37.css',
             ),
             'js' => array(
-                '/rozier-backend-theme-cforms.0e7172ab.min.js',
+                '/rozier-backend-theme-cforms.2c81e5e2.min.js',
             ),
         ),
     )
