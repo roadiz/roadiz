0bbebac1a105ba4eb671ab02e8003fa1fda1a9fa
Ambroise Maupate <ambroise@rezo-zero.com>
2017-03-20 09:43:12 +0100

    Fixed contextual menu margin top.

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -4,27 +4,27 @@ return array(
         'global' => array(
             'css' => array(
                 '/vendor.303ca180.css',
-                '/style.a813c248.css',
+                '/style.19d1b826.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.6199439a.min.js',
-                '/rozier-backend-theme-vendor.59f034fb.min.js',
+                '/rozier-backend-theme.e7fa941f.min.js',
+                '/rozier-backend-theme-vendor.fde7253c.min.js',
             ),
         ),
         'simple' => array(
             'css' => array(
             ),
             'js' => array(
-                '/rozier-backend-theme-simple.b5fa2685.min.js',
+                '/rozier-backend-theme-simple.b3509f4a.min.js',
             ),
         ),
         'custom-forms' => array(
             'css' => array(
                 '/vendor.303ca180.css',
-                '/custom-forms-front.55adca37.css',
+                '/custom-forms-front.c184816a.css',
             ),
             'js' => array(
-                '/rozier-backend-theme-cforms.2c81e5e2.min.js',
+                '/rozier-backend-theme-cforms.3f5f6e91.min.js',
             ),
         ),
     )
