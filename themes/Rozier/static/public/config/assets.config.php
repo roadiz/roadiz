ba495334ef682ee71a553f6b2c1687728519fd6b
Adrien Scholaert <scholaert@gmail.com>
2017-04-19 11:46:17 +0200

    Build files

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -7,16 +7,16 @@ return array(
                 '/style.848dfd87.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.f458c69c.min.js',
-                '/rozier-backend-theme-vendor.20e2e7b4.min.js',
-                '/vue-bundle.e0ad6d93.min.js',
+                '/rozier-backend-theme.4fc6dcf0.min.js',
+                '/rozier-backend-theme-vendor.79553d54.min.js',
+                '/vue-bundle.416bdad8.min.js',
             ),
         ),
         'simple' => array(
             'css' => array(
             ),
             'js' => array(
-                '/rozier-backend-theme-simple.4defc069.min.js',
+                '/rozier-backend-theme-simple.309305ac.min.js',
             ),
         ),
         'custom-forms' => array(
@@ -25,7 +25,7 @@ return array(
                 '/custom-forms-front.41b6902e.css',
             ),
             'js' => array(
-                '/rozier-backend-theme-cforms.3b2654d5.min.js',
+                '/rozier-backend-theme-cforms.a4da59f7.min.js',
             ),
         ),
     )
