fbf377eb849bcb750b3aeaf6926de71cc3404331
Ambroise Maupate <ambroise@rezo-zero.com>
2017-04-25 11:29:26 +0200

    Added max-width for percent inputs.

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -3,12 +3,12 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-                '/vendor.5c6b2574.css',
-                '/style.848dfd87.css',
+                '/vendor.303ca180.css',
+                '/style.1cddbd54.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.8cd02731.min.js',
-                '/rozier-backend-theme-vendor.b0cf653b.min.js',
+                '/rozier-backend-theme.dc1b4394.min.js',
+                '/rozier-backend-theme-vendor.0dab4bdb.min.js',
                 '/vue-bundle.cad7fda6.min.js',
             ),
         ),
@@ -16,16 +16,16 @@ return array(
             'css' => array(
             ),
             'js' => array(
-                '/rozier-backend-theme-simple.0d6015b1.min.js',
+                '/rozier-backend-theme-simple.adbccf48.min.js',
             ),
         ),
         'custom-forms' => array(
             'css' => array(
-                '/vendor.5c6b2574.css',
-                '/custom-forms-front.41b6902e.css',
+                '/vendor.303ca180.css',
+                '/custom-forms-front.0b8e71ff.css',
             ),
             'js' => array(
-                '/rozier-backend-theme-cforms.41d9131a.min.js',
+                '/rozier-backend-theme-cforms.c80c8a08.min.js',
             ),
         ),
     )
