cbc347cffe9b0c01bf7cf2c749110f72e9f51591
Adrien Scholaert <scholaert@gmail.com>
2017-05-23 14:15:34 +0200

    Build files

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -3,29 +3,29 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-                '/vendor.303ca180.css',
+                '/vendor.5c6b2574.css',
                 '/style.1cddbd54.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.f0e32209.min.js',
-                '/rozier-backend-theme-vendor.2e7fa587.min.js',
-                '/vue-bundle.cad7fda6.min.js',
+                '/rozier-backend-theme.2784ffe7.min.js',
+                '/rozier-backend-theme-vendor.997dec28.min.js',
+                '/vue-bundle.f47d793a.min.js',
             ),
         ),
         'simple' => array(
             'css' => array(
             ),
             'js' => array(
-                '/rozier-backend-theme-simple.8a818384.min.js',
+                '/rozier-backend-theme-simple.37e74884.min.js',
             ),
         ),
         'custom-forms' => array(
             'css' => array(
-                '/vendor.303ca180.css',
+                '/vendor.5c6b2574.css',
                 '/custom-forms-front.203d7f02.css',
             ),
             'js' => array(
-                '/rozier-backend-theme-cforms.509ec9e0.min.js',
+                '/rozier-backend-theme-cforms.ed67d7e2.min.js',
             ),
         ),
     )
