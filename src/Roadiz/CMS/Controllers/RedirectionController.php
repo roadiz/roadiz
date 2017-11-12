2757e3f907f92b429b6919738c3278e4eb2ec41f
Ambroise Maupate <ambroise@rezo-zero.com>
2017-11-12 16:15:01 +0100

    Fixed non-root AssetPackages base path resolution.

--- src/Roadiz/CMS/Controllers/RedirectionController.php
+++ src/Roadiz/CMS/Controllers/RedirectionController.php
@@ -31,7 +31,10 @@ namespace RZ\Roadiz\CMS\Controllers;
 
 use RZ\Roadiz\Core\Entities\Redirection;
 use RZ\Roadiz\Core\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 /**
  * Class RedirectionController
@@ -55,5 +58,39 @@ class RedirectionController extends AppController
         }
 
         throw new ResourceNotFoundException();
+    }
+
+    /**
+     * Redirects to another route with the given name.
+     *
+     * The response status code is 302 if the permanent parameter is false (default),
+     * and 301 if the redirection is permanent.
+     *
+     * In case the route name is empty, the status code will be 404 when permanent is false
+     * and 410 otherwise.
+     *
+     * @param Request    $request          The request instance
+     * @param string     $route            The route name to redirect to
+     * @param bool       $permanent        Whether the redirection is permanent
+     * @param bool|array $ignoreAttributes Whether to ignore attributes or an array of attributes to ignore
+     *
+     * @return RedirectResponse A Response instance
+     *
+     * @throws HttpException In case the route name is empty
+     */
+    public function redirectToRouteAction(Request $request, $route, $permanent = false, $ignoreAttributes = false)
+    {
+        if ('' == $route) {
+            throw new HttpException($permanent ? 410 : 404);
+        }
+        $attributes = array();
+        if (false === $ignoreAttributes || is_array($ignoreAttributes)) {
+            $attributes = $request->attributes->get('_route_params');
+            unset($attributes['route'], $attributes['permanent'], $attributes['ignoreAttributes']);
+            if ($ignoreAttributes) {
+                $attributes = array_diff_key($attributes, array_flip($ignoreAttributes));
+            }
+        }
+        return new RedirectResponse($this->generateUrl($route, $attributes, UrlGeneratorInterface::ABSOLUTE_URL), $permanent ? 301 : 302);
     }
 }
