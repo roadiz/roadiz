0e6d864fb5cdbc5709d8768e3c77063870468c64
Adrien Scholaert <adrien@rezo-zero.com>
2017-11-22 17:34:24 +0100

    Cherry pick - update critical VueJS

--- themes/Rozier/static/public/config/assets.config.php
+++ themes/Rozier/static/public/config/assets.config.php
@@ -3,29 +3,29 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-                '/vendor.5c6b2574.css',
+                '/vendor.bb41d2c2.css',
                 '/style.6c74be86.css',
             ),
             'js' => array(
-                '/rozier-backend-theme.5be2ab5a.min.js',
-                '/rozier-backend-theme-vendor.39bfe46a.min.js',
-                '/vue-bundle.49f1a9e6.min.js',
+                '/rozier-backend-theme.655ab647.min.js',
+                '/rozier-backend-theme-vendor.9db3b715.min.js',
+                '/vue-bundle.ac0ff998.min.js',
             ),
         ),
         'simple' => array(
             'css' => array(
             ),
             'js' => array(
-                '/rozier-backend-theme-simple.7078bc12.min.js',
+                '/rozier-backend-theme-simple.fa5e722e.min.js',
             ),
         ),
         'custom-forms' => array(
             'css' => array(
-                '/vendor.5c6b2574.css',
+                '/vendor.bb41d2c2.css',
                 '/custom-forms-front.7455d1eb.css',
             ),
             'js' => array(
-                '/rozier-backend-theme-cforms.8c6a25cc.min.js',
+                '/rozier-backend-theme-cforms.c7015aa4.min.js',
             ),
         ),
     )
