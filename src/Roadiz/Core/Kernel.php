90875abad1a272d9c3593a6c8d23a9190865f78d
Ambroise Maupate <ambroise@rezo-zero.com>
2019-03-18 20:08:46 +0100

    Bumped

--- src/Roadiz/Core/Kernel.php
+++ src/Roadiz/Core/Kernel.php
@@ -90,7 +90,7 @@ class Kernel implements ServiceProviderInterface, KernelInterface, TerminableInt
     const INSTALL_CLASSNAME = InstallApp::class;
 
     public static $cmsBuild = null;
-    public static $cmsVersion = "0.22.23";
+    public static $cmsVersion = "0.22.24";
 
     /**
      * @var Container|null
