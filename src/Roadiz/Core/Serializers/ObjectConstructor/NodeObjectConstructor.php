0056fc96b0a163b81c356899b9036a62e1956999
Ambroise Maupate <ambroise@rezo-zero.com>
2019-07-02 02:29:58 +0200

    Added Deserialize object constructors for Node and NodeType

---
+++ src/Roadiz/Core/Serializers/ObjectConstructor/NodeObjectConstructor.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Copyright (c) 2019. Ambroise Maupate and Julien Blanchet
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * Except as contained in this notice, the name of the ROADIZ shall not
+ * be used in advertising or otherwise to promote the sale, use or other dealings
+ * in this Software without prior written authorization from Ambroise Maupate and Julien Blanchet.
+ *
+ * @file NodeObjectConstructor.php
+ * @author Ambroise Maupate <ambroise@rezo-zero.com>
+ */
+
+namespace RZ\Roadiz\Core\Serializers\ObjectConstructor;
+
+use JMS\Serializer\Exception\ObjectConstructionException;
+use RZ\Roadiz\Core\Entities\Node;
+
+class NodeObjectConstructor extends AbstractTypedObjectConstructor
+{
+    /**
+     * @inheritDoc
+     */
+    public function supports(string $className, array $data): bool
+    {
+        return $className === Node::class && array_key_exists('nodeName', $data);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function findObject($data): ?object
+    {
+        if (null === $data['nodeName'] || $data['nodeName'] === '') {
+            throw new ObjectConstructionException('Node name can not be empty');
+        }
+        return $this->entityManager
+            ->getRepository(Node::class)
+            ->setDisplayingAllNodesStatuses(true)
+            ->findOneByNodeName($data['nodeName']);
+    }
+}
