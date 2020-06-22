f01de0e20496e186e74ff0421265a66663110feb
Ambroise Maupate <ambroise@rezo-zero.com>
2020-06-22 18:29:41 +0200

    Fixed Monster SQL query for fetching latest logs

--- src/Roadiz/Core/Repositories/LogRepository.php
+++ src/Roadiz/Core/Repositories/LogRepository.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace RZ\Roadiz\Core\Repositories;
 
+use Doctrine\ORM\Query;
 use Doctrine\ORM\Tools\Pagination\Paginator;
 
 class LogRepository extends EntityRepository
@@ -15,12 +16,31 @@ class LogRepository extends EntityRepository
      */
     public function findLatestByNodesSources($maxResult = 5)
     {
-        $query = $this->createQueryBuilder('log');
-        $query->innerJoin('log.nodeSource', 'ns')
-            ->addGroupBy('log')
+        /*
+         * We need to split this query in 2 for performance matter.
+         */
+        $qb = $this->createQueryBuilder('log');
+        $qb->select('log.id');
+        $qb->innerJoin('log.nodeSource', 'ns')
+            ->addGroupBy('log.id')
             ->orderBy('log.datetime', 'DESC')
-            ->setMaxResults($maxResult);
+            ->setMaxResults($maxResult)
+        ;
+        $ids = $qb->getQuery()
+            ->setHint(Query::HINT_FORCE_PARTIAL_LOAD, true)
+            ->getScalarResult();
 
-        return new Paginator($query->getQuery(), true);
+
+        $qb2 = $this->createQueryBuilder('log');
+        $qb2->addSelect('ns, n, dbf')
+            ->andWhere($qb2->expr()->in('log.id', ':id'))
+            ->innerJoin('log.nodeSource', 'ns')
+            ->innerJoin('ns.documentsByFields', 'dbf')
+            ->innerJoin('ns.node', 'n')
+            ->setParameter(':id', array_map(function (array $item) {
+                return $item['id'];
+            }, $ids));
+
+        return new Paginator($qb2->getQuery(), true);
     }
 }
