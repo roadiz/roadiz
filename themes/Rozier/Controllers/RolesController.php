dc329c0b831a7032d1948794aac7478574b318a3
Ambroise Maupate <ambroise@rezo-zero.com>
2020-11-17 11:27:08 +0100

    Use dedicated form type for Roles

--- themes/Rozier/Controllers/RolesController.php
+++ themes/Rozier/Controllers/RolesController.php
@@ -8,14 +8,14 @@ use RZ\Roadiz\Core\Entities\Role;
 use RZ\Roadiz\Core\Exceptions\EntityAlreadyExistsException;
 use RZ\Roadiz\Core\Exceptions\EntityRequiredException;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\NotNull;
-use Symfony\Component\Validator\Constraints\Regex;
+use Themes\Rozier\Forms\RoleType;
 use Themes\Rozier\RozierApp;
 use Themes\Rozier\Utils\SessionListFilters;
 
@@ -66,24 +66,24 @@ class RolesController extends RozierApp
     {
         $this->denyAccessUnlessGranted('ROLE_ACCESS_ROLES');
 
-        $form = $this->buildAddForm();
+        $role = new Role('ROLE_EXAMPLE');
+        $form = $this->createForm(RoleType::class, $role);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             try {
-                $role = $this->addRole($form->getData());
+                $role = $this->addRole($role);
                 $msg = $this->getTranslator()->trans(
                     'role.%name%.created',
-                    ['%name%' => $role->getName()]
+                    ['%name%' => $role->getRole()]
                 );
                 $this->publishConfirmMessage($request, $msg);
+                return $this->redirect($this->generateUrl('rolesHomePage'));
             } catch (EntityAlreadyExistsException $e) {
-                $this->publishErrorMessage($request, $e->getMessage());
+                $form->addError(new FormError($e->getMessage()));
             } catch (RuntimeException $e) {
-                $this->publishErrorMessage($request, $e->getMessage());
+                $form->addError(new FormError($e->getMessage()));
             }
-
-            return $this->redirect($this->generateUrl('rolesHomePage'));
         }
 
         $this->assignation['form'] = $form->createView();
@@ -123,13 +123,12 @@ class RolesController extends RozierApp
                         ['%name%' => $role->getName()]
                     );
                     $this->publishConfirmMessage($request, $msg);
+                    return $this->redirect($this->generateUrl('rolesHomePage'));
                 } catch (EntityRequiredException $e) {
-                    $this->publishErrorMessage($request, $e->getMessage());
+                    $form->addError(new FormError($e->getMessage()));
                 } catch (RuntimeException $e) {
-                    $this->publishErrorMessage($request, $e->getMessage());
+                    $form->addError(new FormError($e->getMessage()));
                 }
-
-                return $this->redirect($this->generateUrl('rolesHomePage'));
             }
 
             $this->assignation['form'] = $form->createView();
@@ -152,35 +151,32 @@ class RolesController extends RozierApp
     {
         $this->denyAccessUnlessGranted('ROLE_ACCESS_ROLES');
 
-        /** @var Role $role */
-        $role = $this->get('em')
-                     ->find(Role::class, (int) $roleId);
+        /** @var Role|null $role */
+        $role = $this->get('em')->find(Role::class, (int) $roleId);
 
-        if ($role !== null &&
-            !$role->required()) {
+        if ($role !== null && !$role->required()) {
             if (!$this->isGranted($role->getRole())) {
                 throw $this->createAccessDeniedException('You cannot edit a role you do not have.');
             }
-            $form = $this->buildEditForm($role);
+            $form = $this->createForm(RoleType::class, $role);
             $form->handleRequest($request);
 
-            if ($form->isSubmitted() &&
-                $form->isValid() &&
-                $form->getData()['roleId'] == $role->getId()) {
+            if ($form->isSubmitted() && $form->isValid()) {
                 try {
-                    $this->editRole($form->getData(), $role);
+                    $this->editRole($role);
                     $msg = $this->getTranslator()->trans(
                         'role.%name%.updated',
-                        ['%name%' => $role->getName()]
+                        ['%name%' => $role->getRole()]
                     );
                     $this->publishConfirmMessage($request, $msg);
+                    return $this->redirect($this->generateUrl('rolesHomePage'));
+                } catch (EntityRequiredException $e) {
+                    $form->addError(new FormError($e->getMessage()));
                 } catch (EntityAlreadyExistsException $e) {
-                    $this->publishErrorMessage($request, $e->getMessage());
+                    $form->addError(new FormError($e->getMessage()));
                 } catch (RuntimeException $e) {
-                    $this->publishErrorMessage($request, $e->getMessage());
+                    $form->addError(new FormError($e->getMessage()));
                 }
-
-                return $this->redirect($this->generateUrl('rolesHomePage'));
             }
 
             $this->assignation['form'] = $form->createView();
@@ -190,29 +186,6 @@ class RolesController extends RozierApp
         }
 
         throw new ResourceNotFoundException();
-    }
-
-    /**
-     * Build add role form with name constraint.
-     *
-     * @return FormInterface
-     */
-    protected function buildAddForm()
-    {
-        $builder = $this->createFormBuilder()
-                        ->add('name', TextType::class, [
-                            'label' => 'name',
-                            'constraints' => [
-                                new NotNull(),
-                                new NotBlank(),
-                                new Regex([
-                                    'pattern' => '#^ROLE_([A-Z0-9\_]+)$#',
-                                    'message' => 'role.name.must_comply_with_standard',
-                                ]),
-                            ],
-                        ]);
-
-        return $builder->getForm();
     }
 
     /**
@@ -237,107 +210,60 @@ class RolesController extends RozierApp
     }
 
     /**
-     * Build edit role form with name constraint.
-     *
-     * @param Role $role
-     *
-     * @return FormInterface
-     */
-    protected function buildEditForm(Role $role)
-    {
-        $defaults = [
-            'name' => $role->getName(),
-        ];
-        $builder = $this->createFormBuilder($defaults)
-                        ->add('roleId', HiddenType::class, [
-                            'data' => $role->getId(),
-                            'constraints' => [
-                                new NotNull(),
-                                new NotBlank(),
-                            ],
-                        ])
-                        ->add('name', TextType::class, [
-                            'data' => $role->getName(),
-                            'label' => 'name',
-                            'constraints' => [
-                                new Regex([
-                                    'pattern' => '#^ROLE_([A-Z\_]+)$#',
-                                    'message' => 'role.name.must_comply_with_standard',
-                                ]),
-                            ],
-                        ]);
-
-        return $builder->getForm();
-    }
-
-    /**
-     * @param array $data
-     *
+     * @param Role $newRole
      * @return Role
-     * @throws EntityAlreadyExistsException
      */
-    protected function addRole(array $data)
+    protected function addRole(Role $newRole)
     {
-        if (isset($data['name'])) {
-            $existing = $this->get('em')
-                             ->getRepository(Role::class)
-                             ->findOneBy(['name' => $data['name']]);
-            if ($existing !== null) {
-                throw new EntityAlreadyExistsException($this->getTranslator()->trans("role.name.already.exists"), 1);
-            }
-
-            $role = new Role($data['name']);
-            $this->get('em')->persist($role);
-            $this->get('em')->flush();
-
-            // Clear result cache
-            $cacheDriver = $this->get('em')->getConfiguration()->getResultCacheImpl();
-            if ($cacheDriver !== null) {
-                $cacheDriver->deleteAll();
-            }
-
-            return $role;
-        } else {
-            throw new RuntimeException("Role name is not defined", 1);
+        $existing = $this->get('em')
+                         ->getRepository(Role::class)
+                         ->findOneBy(['name' => $newRole->getRole()]);
+        if ($existing !== null) {
+            throw new EntityAlreadyExistsException($this->getTranslator()->trans("role.name.already.exists"), 1);
         }
+
+        $this->get('em')->persist($newRole);
+        $this->get('em')->flush();
+
+        // Clear result cache
+        $cacheDriver = $this->get('em')->getConfiguration()->getResultCacheImpl();
+        if ($cacheDriver !== null) {
+            $cacheDriver->deleteAll();
+        }
+
+        return $newRole;
     }
 
     /**
-     * @param array $data
      * @param Role  $role
      *
      * @return Role
      * @throws EntityAlreadyExistsException
      * @throws EntityRequiredException
      */
-    protected function editRole(array $data, Role $role)
+    protected function editRole(Role $role)
     {
         if ($role->required()) {
             throw new EntityRequiredException($this->getTranslator()->trans("role.required.cannot_be_updated"), 1);
         }
 
-        if (isset($data['name'])) {
-            $existing = $this->get('em')
-                             ->getRepository(Role::class)
-                             ->findOneBy(['name' => $data['name']]);
-            if ($existing !== null &&
-                $existing->getId() != $role->getId()) {
-                throw new EntityAlreadyExistsException($this->getTranslator()->trans("role.name.already.exists"), 1);
-            }
-
-            $role->setName($data['name']);
-            $this->get('em')->flush();
-
-            // Clear result cache
-            $cacheDriver = $this->get('em')->getConfiguration()->getResultCacheImpl();
-            if ($cacheDriver !== null) {
-                $cacheDriver->deleteAll();
-            }
-
-            return $role;
-        } else {
-            throw new RuntimeException("Role name is not defined", 1);
+        $existing = $this->get('em')
+                         ->getRepository(Role::class)
+                         ->findOneBy(['name' => $role->getRole()]);
+        if ($existing !== null &&
+            $existing->getId() !== $role->getId()) {
+            throw new EntityAlreadyExistsException($this->getTranslator()->trans("role.name.already.exists"), 1);
         }
+
+        $this->get('em')->flush();
+
+        // Clear result cache
+        $cacheDriver = $this->get('em')->getConfiguration()->getResultCacheImpl();
+        if ($cacheDriver !== null) {
+            $cacheDriver->deleteAll();
+        }
+
+        return $role;
     }
 
     /**
