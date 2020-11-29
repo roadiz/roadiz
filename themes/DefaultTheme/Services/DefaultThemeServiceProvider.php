2da8a17edc9f04163cc8cc2f30255c14fe215777
Ambroise Maupate <ambroise@rezo-zero.com>
2020-11-29 21:51:00 +0100

    Use service provider for default theme

---
+++ themes/DefaultTheme/Services/DefaultThemeServiceProvider.php
@@ -0,0 +1,66 @@
+<?php
+declare(strict_types=1);
+
+namespace Themes\DefaultTheme\Services;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+use RZ\Roadiz\Core\Events\NodesSources\NodesSourcesIndexingEvent;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+use Themes\DefaultTheme\Event\LinkPathSubscriber;
+use Themes\DefaultTheme\Serialization\DocumentUriSubscriber;
+use Themes\DefaultTheme\Twig\ImageFormatsExtension;
+
+class DefaultThemeServiceProvider implements ServiceProviderInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function register(Container $container)
+    {
+        $container->extend('twig.extensions', function (ArrayCollection $extensions) {
+            $extensions->add(new ImageFormatsExtension());
+            return $extensions;
+        });
+
+        $container->extend('serializer.subscribers', function ($subscribers, Container $c) {
+            $subscribers[] = new DocumentUriSubscriber($c);
+            return $subscribers;
+        });
+
+        $container->extend('backoffice.entries', function (array $entries, Container $c) {
+            /*
+             * Add a test entry in your Backoffice
+             * Remove this in your theme if you don’t
+             * want to extend Back-office
+             */
+            $entries['test'] = [
+                'name' => 'test',
+                'path' => $c['urlGenerator']->generate('adminTestPage'),
+                'icon' => 'uk-icon-cube',
+                'roles' => null,
+                'subentries' => null,
+            ];
+
+            return $entries;
+        });
+
+        /*
+         * Example:
+         * Alter Solr indexing with custom data.
+         */
+        $container->extend('dispatcher', function (EventDispatcherInterface $dispatcher) {
+            $dispatcher->addSubscriber(new LinkPathSubscriber());
+            $dispatcher->addListener(
+                NodesSourcesIndexingEvent::class,
+                function (NodesSourcesIndexingEvent $event) {
+                    $assoc = $event->getAssociations();
+                    $assoc['defaulttheme_txt'] = 'This is injected by Default theme during indexing.';
+                    $event->setAssociations($assoc);
+                }
+            );
+            return $dispatcher;
+        });
+    }
+}
