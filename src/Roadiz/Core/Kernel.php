81f4cb73d14f7fff32f8cf83774a61616f87a76a
Ambroise Maupate <ambroise@rezo-zero.com>
2020-12-04 16:53:24 +0100

    Force doctrine/orm in 2.7 due to BC

--- src/Roadiz/Core/Kernel.php
+++ src/Roadiz/Core/Kernel.php
@@ -107,7 +107,7 @@ class Kernel implements ServiceProviderInterface, KernelInterface, RebootableInt
     const SECURITY_DOMAIN = 'roadiz_domain';
     const INSTALL_CLASSNAME = InstallApp::class;
     public static $cmsBuild = null;
-    public static $cmsVersion = "1.5.14";
+    public static $cmsVersion = "1.5.15";
 
     protected $environment;
     protected $debug;
