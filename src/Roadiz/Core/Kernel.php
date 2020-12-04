a725e8ae1bc16a7a484f0cd3bf8c1bbc6df83ab9
Ambroise Maupate <ambroise@rezo-zero.com>
2020-12-04 17:05:08 +0100

    Fixed doctrine/orm

--- src/Roadiz/Core/Kernel.php
+++ src/Roadiz/Core/Kernel.php
@@ -128,7 +128,7 @@ class Kernel implements ServiceProviderInterface, KernelInterface, RebootableInt
     const SECURITY_DOMAIN = 'roadiz_domain';
     const INSTALL_CLASSNAME = InstallApp::class;
     public static $cmsBuild = null;
-    public static $cmsVersion = "1.3.22";
+    public static $cmsVersion = "1.3.23";
 
     protected $environment;
     protected $debug;
