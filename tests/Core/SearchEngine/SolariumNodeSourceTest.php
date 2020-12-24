1e9e917748e95b815df40857fbd7a51b0dbdafd5
Ambroise Maupate <ambroise@rezo-zero.com>
2020-12-24 22:28:55 +0100

    Skip test when no data

--- tests/Core/SearchEngine/SolariumNodeSourceTest.php
+++ tests/Core/SearchEngine/SolariumNodeSourceTest.php
@@ -1,32 +1,4 @@
 <?php
-/**
- * Copyright © 2015, Ambroise Maupate and Julien Blanchet
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is furnished
- * to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- * IN THE SOFTWARE.
- *
- * Except as contained in this notice, the name of the ROADIZ shall not
- * be used in advertising or otherwise to promote the sale, use or other dealings
- * in this Software without prior written authorization from Ambroise Maupate and Julien Blanchet.
- *
- * @file SolariumNodeSourceTest.php
- * @author Ambroise Maupate
- */
 
 use RZ\Roadiz\Core\Entities\NodesSources;
 use RZ\Roadiz\Core\Exceptions\SolrServerNotAvailableException;
@@ -35,9 +7,6 @@ use RZ\Roadiz\Core\SearchEngine\SolariumFactoryInterface;
 use RZ\Roadiz\Tests\DefaultThemeWithNodesDependentCase;
 use Solarium\Exception\HttpException;
 
-/**
- * SolariumNodeSourceTest.
- */
 class SolariumNodeSourceTest extends DefaultThemeWithNodesDependentCase
 {
     private static $entityCollection;
@@ -85,6 +54,8 @@ class SolariumNodeSourceTest extends DefaultThemeWithNodesDependentCase
             } catch (HttpException $e) {
                 $this->markTestSkipped($e->getMessage());
             }
+        } else {
+            $this->markTestSkipped('Ipsum Lorem Vehicula node does not exist.');
         }
     }
 
@@ -112,6 +83,8 @@ class SolariumNodeSourceTest extends DefaultThemeWithNodesDependentCase
             } catch (HttpException $e) {
                 $this->markTestSkipped($e->getMessage());
             }
+        } else {
+            $this->markTestSkipped('Ipsum Lorem Vehicula node does not exist.');
         }
     }
 
@@ -142,6 +115,8 @@ class SolariumNodeSourceTest extends DefaultThemeWithNodesDependentCase
             } catch (HttpException $e) {
                 $this->markTestSkipped($e->getMessage());
             }
+        } else {
+            $this->markTestSkipped('Ipsum Lorem Vehicula node does not exist.');
         }
     }
 
