e5872f42a5b55c4fc654f3577e60ad1f1555150a
Ambroise Maupate <ambroise@rezo-zero.com>
2021-05-20 23:33:50 +0200

    Try to set password for travis mariadb

--- .travis/php_unit_install.sh
+++ .travis/php_unit_install.sh
@@ -8,9 +8,17 @@ then
     cp conf/config.pgsql.travis.yml conf/config.yml || exit 1;
 fi;
 
-if [ "$DB" = 'mysql' ] || [ "$DB" = 'mariadb' ];
+if [ "$DB" = 'mysql' ];
+then
+    mysql -e 'create database IF NOT EXISTS roadiz_tests_tmp; create database IF NOT EXISTS roadiz_tests;' || exit 1;
+    cp conf/config.mysql.travis.yml conf/config.yml || exit 1;
+fi;
+
+if [ "$DB" = 'mariadb' ];
 then
     sudo mysql -e 'create database IF NOT EXISTS roadiz_tests_tmp; create database IF NOT EXISTS roadiz_tests;' || exit 1;
+    sudo mysql -e "SET Password=PASSWORD('${MYSQL_PASSWORD}');" || exit 1;
+    sudo service mysql restart
     cp conf/config.mysql.travis.yml conf/config.yml || exit 1;
 fi;
 
