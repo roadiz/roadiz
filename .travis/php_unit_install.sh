5328cf8f6c6bf834d5f3db4825b9ac92fd2809f5
Ambroise Maupate <ambroise@rezo-zero.com>
2021-05-20 23:20:56 +0200

    Travis mariadb before script

--- .travis/php_unit_install.sh
+++ .travis/php_unit_install.sh
@@ -1,19 +1,19 @@
 #!/bin/sh -x
 if [ "$DB" = 'pgsql' ];
 then
-    psql -c 'DROP DATABASE IF EXISTS roadiz_tests;' -U postgres;
-    psql -c 'DROP DATABASE IF EXISTS roadiz_tests_tmp;' -U postgres;
-    psql -c 'create database roadiz_tests;' -U postgres;
-    psql -c 'create database roadiz_tests_tmp;' -U postgres;
-    cp conf/config.pgsql.travis.yml conf/config.yml;
+    psql -c 'DROP DATABASE IF EXISTS roadiz_tests;' -U postgres || exit 1;
+    psql -c 'DROP DATABASE IF EXISTS roadiz_tests_tmp;' -U postgres || exit 1;
+    psql -c 'create database roadiz_tests;' -U postgres || exit 1;
+    psql -c 'create database roadiz_tests_tmp;' -U postgres || exit 1;
+    cp conf/config.pgsql.travis.yml conf/config.yml || exit 1;
 fi;
 
 if [ "$DB" = 'mysql' ] || [ "$DB" = 'mariadb' ];
 then
-    mysql -e 'create database IF NOT EXISTS roadiz_tests_tmp; create database IF NOT EXISTS roadiz_tests;';
-    cp conf/config.mysql.travis.yml conf/config.yml;
+    sudo mysql -e 'create database IF NOT EXISTS roadiz_tests_tmp; create database IF NOT EXISTS roadiz_tests;' || exit 1;
+    cp conf/config.mysql.travis.yml conf/config.yml || exit 1;
 fi;
 
 phpenv config-rm xdebug.ini;
-curl -s http://getcomposer.org/installer | php;
-php composer.phar install --dev --no-interaction;
+curl -s http://getcomposer.org/installer | php || exit 1;
+php composer.phar install --dev --no-interaction || exit 1;
