97936c76b76333a6f84178e325d4c705a4a3066a
Ambroise Maupate <ambroise@rezo-zero.com>
2021-06-05 11:58:15 +0200

    Renamed userProvider service to UserProviderInterface::class in order to use ChainUserProvider, Fixes #389
    Add detail on AuthCollector for SwitchUserToken
    
    Typo
    
    Renamed userProvider service to UserProviderInterface::class in order to use ChainUserProvider, Fixes #389
    
    UserProvider

--- src/Roadiz/Utils/DebugBar/DataCollector/AuthCollector.php
+++ src/Roadiz/Utils/DebugBar/DataCollector/AuthCollector.php
@@ -8,6 +8,8 @@ use DebugBar\DataCollector\Renderable;
 use RZ\Roadiz\Core\Entities\User;
 use RZ\Roadiz\OpenId\User\OpenIdAccount;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+use Symfony\Component\Security\Core\Authentication\Token\SwitchUserToken;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 final class AuthCollector extends DataCollector implements Renderable
@@ -22,65 +24,84 @@ final class AuthCollector extends DataCollector implements Renderable
         $this->tokenStorage = $tokenStorage;
     }
 
+    private function getTokenInfo(?TokenInterface $token): array
+    {
+        if (null === $token) {
+            return [
+                'name' => 'Guest',
+                'user' => [],
+            ];
+        }
+
+        $user = $token->getUser();
+
+        if (null !== $user && $user instanceof OpenIdAccount) {
+            return [
+                'name' => $user->getEmail(),
+                'user' => [
+                    'Token' => get_class($token),
+                    'Token roles' => $token->getRoleNames(),
+                    'Issuer' => $user->getIssuer(),
+                    'User roles' => $user->getRoles(),
+                    'Email' => $user->getEmail(),
+                    'Name' => $user->getName(),
+                    'Given name' => $user->getGivenName(),
+                    'Family name' => $user->getFamilyName(),
+                    'Phone number' => $user->getPhoneNumber(),
+                    'Profile' => $user->getProfile(),
+                    'Middle name' => $user->getMiddleName(),
+                ]
+            ];
+        } elseif (null !== $user && $user instanceof User) {
+            return [
+                'name' => $user->getUsername(),
+                'user' => [
+                    'Token' => get_class($token),
+                    'Token roles' => $token->getRoleNames(),
+                    'User roles' => $user->getRoles(),
+                    'Email' => $user->getEmail(),
+                    'Last login' => $user->getLastLogin() ? $user->getLastLogin()->format("Y-m-d H:i:s") : null,
+                ]
+            ];
+        } elseif (null !== $user && $user instanceof UserInterface) {
+            return [
+                'name' => $user->getUsername(),
+                'user' => [
+                    'Token' => get_class($token),
+                    'Token roles' => $token->getRoleNames(),
+                    'User roles' => $user->getRoles()
+                ]
+            ];
+        } else {
+            return [
+                'name' => 'Guest',
+                'user' => [
+                    'Token' => get_class($token),
+                    'Roles' =>  $token->getRoleNames(),
+                ]
+            ];
+        }
+    }
+
     /**
      * @inheritDoc
      */
     public function collect()
     {
-        if (null !== $this->tokenStorage->getToken()) {
-            $user = $this->tokenStorage->getToken()->getUser();
-            if (null !== $user && $user instanceof OpenIdAccount) {
-                return [
-                    'name' => $user->getEmail(),
-                    'user' => [
-                        'Token' => get_class($this->tokenStorage->getToken()),
-                        'Token roles' => $this->tokenStorage->getToken()->getRoleNames(),
-                        'Issuer' => $user->getIssuer(),
-                        'User roles' => $user->getRoles(),
-                        'Email' => $user->getEmail(),
-                        'Name' => $user->getName(),
-                        'Given name' => $user->getGivenName(),
-                        'Family name' => $user->getFamilyName(),
-                        'Phone number' => $user->getPhoneNumber(),
-                        'Profile' => $user->getProfile(),
-                        'Middle name' => $user->getMiddleName(),
-                    ]
-                ];
-            } elseif (null !== $user && $user instanceof User) {
-                return [
-                    'name' => $user->getUsername(),
-                    'user' => [
-                        'Token' => get_class($this->tokenStorage->getToken()),
-                        'Token roles' => $this->tokenStorage->getToken()->getRoleNames(),
-                        'User roles' => $user->getRoles(),
-                        'Email' => $user->getEmail(),
-                        'Last login' => $user->getLastLogin() ? $user->getLastLogin()->format("Y-m-d H:i:s") : null,
-                    ]
-                ];
-            } elseif (null !== $user && $user instanceof UserInterface) {
-                return [
-                    'name' => $user->getUsername(),
-                    'user' => [
-                        'Token' => get_class($this->tokenStorage->getToken()),
-                        'Token roles' => $this->tokenStorage->getToken()->getRoleNames(),
-                        'User roles' => $user->getRoles()
-                    ]
-                ];
-            } else {
-                return [
-                    'name' => 'Guest',
-                    'user' => [
-                        'Token' => get_class($this->tokenStorage->getToken()),
-                        'Roles' =>  $this->tokenStorage->getToken()->getRoleNames(),
-                    ]
-                ];
+        $token = $this->tokenStorage->getToken();
+        $info = $this->getTokenInfo($this->tokenStorage->getToken());
+
+        if (null !== $token && $token instanceof SwitchUserToken) {
+            $user = $token->getOriginalToken()->getUser();
+            $info['name'] .= sprintf(' (%s)', $user->getUsername());
+            $info['user']['Token'] .= sprintf(' (%s)', get_class($token->getOriginalToken()));
+
+            if ($user instanceof User || $user instanceof OpenIdAccount) {
+                $info['user']['Email'] .= sprintf(' (%s)', $user->getEmail());
             }
         }
 
-        return [
-            'name' => 'Guest',
-            'user' => [],
-        ];
+        return $info;
     }
 
     /**
