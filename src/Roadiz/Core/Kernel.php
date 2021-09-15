3c7d3cfc868e4aa4f662b5d1b728a699ec2581d8
Ambroise Maupate <ambroise@rezo-zero.com>
2021-09-15 17:54:09 +0200

    Bumped

--- src/Roadiz/Core/Kernel.php
+++ src/Roadiz/Core/Kernel.php
@@ -107,7 +107,7 @@ class Kernel implements ServiceProviderInterface, KernelInterface, RebootableInt
     const SECURITY_DOMAIN = 'roadiz_domain';
     const INSTALL_CLASSNAME = InstallApp::class;
     public static $cmsBuild = null;
-    public static $cmsVersion = "1.5.28";
+    public static $cmsVersion = "1.5.29";
 
     protected $environment;
     protected $debug;
