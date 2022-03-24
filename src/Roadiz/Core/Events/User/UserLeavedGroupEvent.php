9d5fc98e4699817a35de71accf3b147d2f797c6f
Ambroise Maupate <ambroise@rezo-zero.com>
2022-03-24 14:44:32 +0100

    feat: added new UserJoinedGroupEvent and UserLeavedGroupEvent events

---
+++ src/Roadiz/Core/Events/User/UserLeavedGroupEvent.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RZ\Roadiz\Core\Events\User;
+
+use RZ\Roadiz\Core\Entities\Group;
+use RZ\Roadiz\Core\Entities\User;
+use RZ\Roadiz\Core\Events\FilterUserEvent;
+
+final class UserLeavedGroupEvent extends FilterUserEvent
+{
+    /**
+     * @var Group
+     */
+    private $group;
+
+    public function __construct(User $user, Group $group)
+    {
+        parent::__construct($user);
+        $this->group = $group;
+    }
+
+    /**
+     * @return Group
+     */
+    public function getGroup(): Group
+    {
+        return $this->group;
+    }
+}
